315a6b9c966695fa51ac63a967f758b2ca57c092
Abhishek <kashyapabhi232@gmail.com>
2023-07-19 12:28:37 +0530

    update code

--- src/components/Home/TrendingProducts/index.js
+++ src/components/Home/TrendingProducts/index.js
@@ -1,9 +1,9 @@
 
 import styles from './style.module.css'
 
-const TrendingProducts =()=>{
-    return(<div className={styles.trending_products_wrapper}>
-        Trending Products
-    </div>)
+const TrendingProducts = () => {
+  return (<div className={styles.trending_products_wrapper}>
+    Trending Products
+  </div>)
 }
 export default TrendingProducts;
