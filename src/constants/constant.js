4ae40c2c113e4d91e860f30fd1935e507107960b
Rupali <rupali.shethink@gmail.com>
2023-07-19 20:21:04 +0530

    CIT-12: media coverage section completed

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -1,11 +1,23 @@
 // home page
-import {HomePageImages} from "@/assets/images";
+import {HomePageImages, HomePageMediaCoverage} from "@/assets/images";
 
 export const RentFurniture = [
   {img: HomePageImages.bedroom, label: "Bedroom"},
-  {img: HomePageImages.studyrooom, label: "Study room"},
-  {img: HomePageImages.livingroom, label: "Living room"},
+  {img: HomePageImages.livingroom, label: "Study room"},
+  {img: HomePageImages.studyroom, label: "Living room"},
   {img: HomePageImages.appliances, label: "Appliances"},
   {img: HomePageImages.office, label: "Office"},
   {img: HomePageImages.diningroom, label: "Dining room"},
+];
+
+export const MediaCoverageImages = [
+  {img: HomePageMediaCoverage.icon1},
+  {img: HomePageMediaCoverage.icon2},
+  {img: HomePageMediaCoverage.icon3},
+  {img: HomePageMediaCoverage.icon4},
+  {img: HomePageMediaCoverage.icon5},
+  {img: HomePageMediaCoverage.icon6},
+  {img: HomePageMediaCoverage.icon7},
+  {img: HomePageMediaCoverage.icon8},
+  {img: HomePageMediaCoverage.icon9},
 ];
