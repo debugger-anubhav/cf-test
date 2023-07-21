8bcf32508aaab0aefa3465ecacf4e107d47e4eec
Abhishek <kashyapabhi232@gmail.com>
2023-07-21 12:46:50 +0530

    CIT-9: need to be merge to deve

--- src/app/page.js
+++ src/app/page.js
@@ -27,7 +27,7 @@ export default function Home() {
       <Header />
       <MenuList />
       {/* <HeroBanner /> */}
-      {/* <RentFurnitureAndAppliances /> */}
+      <RentFurnitureAndAppliances />
       <TrendingProducts />
       {/* <WhyCityfurnish /> */}
       {/* <HowProductsLook /> */}
