47f3759f07af0d10e855f3c75d2b75d14f50a7ac
Abhishek <kashyapabhi232@gmail.com>
2023-07-21 12:33:45 +0530

    CIT-14: updated Ui changes

---
+++ src/components/Home/LimetedPreiodDiscount/index.js
@@ -0,0 +1,28 @@
+import Card from "@/components/Common/HomePageCards/Card";
+import styles from "./style.module.css";
+import string from "@/constants/Constant.json";
+import React from "react";
+
+const LimetedPreiodDiscount = () => {
+  const str = string.landing_page.Common_card;
+  return (
+    <div className={styles.main_container}>
+      <h2 className={styles.heading}>Limited period discounts</h2>
+      <h3 className={styles.subHeading}>Hurry before it ends</h3>
+      <div className="flex overflow-x-scroll">
+        {str.map((item, index) => (
+          <div key={index} className="mr-4">
+            <Card
+              desc={item.desc}
+              originalPrice={item.originalPrice}
+              currentPrice={item.currentPrice}
+              discount={item.discount}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default LimetedPreiodDiscount;
