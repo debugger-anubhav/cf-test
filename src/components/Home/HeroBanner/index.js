7ca73206d8babc4da660f6547bf36b09c2eb3c25
Abhishek <kashyapabhi232@gmail.com>
2023-07-25 18:21:09 +0530

    CIT-7 work to be completed

--- src/components/Home/HeroBanner/index.js
+++ src/components/Home/HeroBanner/index.js
@@ -1,46 +1,52 @@
 // import {HeroBannerText} from "@/constants/constant";
+import {HeroBannerImages} from "@/assets/images";
 import styles from "./style.module.css";
 import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Image from "next/image";
 
 const HeroBanner = () => {
+  const settings = {
+    dots: false,
+    infinite: true,
+    arrows: false,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplaySpeed: 1000,
+    // customPaging: function (i) {
+    //   return (
+    //     <div>{i}</div>
+    //   )
+    // }
+  };
   return (
-    <div className={styles.hero_banner_wrapper}>
-      <div className={styles.banner_container}>
-        <div className={styles.heading_container}>
-          {/* <h1 className={styles.heading_text}>{HeroBannerText.heading}</h1> */}
-          <div>
-            <button className={styles.explor_button}>
-              {/* {HeroBannerText.btnText} */}
-            </button>
-          </div>
+    <div className={styles.slick_container}>
+      <Slider {...settings}>
+        <div>
+          <Image
+            src={HeroBannerImages.HeroBannerImageOne}
+            width={328}
+            height={150}
+          />
         </div>
-      </div>
-
-      <div className="flex">
-        <div className={styles.rating_text_container}>
-          <div className="w-fit flex">
-            <div>
-              <p className={styles.rating_heading_text}>
-                {/* {HeroBannerText.subscriberCount} */}
-              </p>
-              <text className={styles.rating_text}>
-                {/* {HeroBannerText.subscriber} */}
-              </text>
-            </div>
-            <div className="ml-2.5 md:ml-10 ">
-              <p className={styles.rating_heading_text}>
-                {/* {HeroBannerText.Google} */}
-              </p>
-              <div className="flex items-center justify-center">
-                <text className={styles.rating_text}>
-                  {/* {HeroBannerText.stars} */}
-                </text>
-                <span className={styles.star_style}></span>
-              </div>
-            </div>
-          </div>
+        <div>
+          <Image
+            src={HeroBannerImages.HeroBannerImageTwo}
+            width={328}
+            height={150}
+          />
         </div>
-      </div>
+        <div>
+          <Image
+            src={HeroBannerImages.HeroBannerImageThree}
+            width={328}
+            height={150}
+          />
+        </div>
+      </Slider>
     </div>
   );
 };
