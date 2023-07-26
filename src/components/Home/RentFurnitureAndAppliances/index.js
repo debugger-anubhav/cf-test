b52496e747bf765caf645dec16f51da21f40adc9
Rupali <rupali.shethink@gmail.com>
2023-07-26 20:07:10 +0530

    Design-fixes: rent furniture section completed for web

--- src/components/Home/RentFurnitureAndAppliances/index.js
+++ src/components/Home/RentFurnitureAndAppliances/index.js
@@ -8,11 +8,24 @@ const RentFurnitureAndAppliances = () => {
   return (
     <div className={styles.rent_furniture_wrapper}>
       <h1 className={styles.head}>{string.landing_page.Rent_furni}</h1>
+      <h2 className={styles.subhead}>{string.landing_page.Explore_by}</h2>
       <div className={styles.card_div}>
         {RentFurniture.map((item, index) => (
           <div key={index}>
-            <Image src={item.img} alt="" className={styles.img} />
-            <p className={styles.label}>{item.label}</p>
+            <div className="relative">
+              <Image src={item.img} alt="" className={styles.img} />
+              <div className={styles.pricetag}>
+                <p className={styles.price}>Starting from</p>
+                <p className={styles.price}>
+                  <span className={styles.span}>{item.pricetag}</span>
+                  month
+                </p>
+              </div>
+            </div>
+            <div className="xl:pl-3 macbook:pl-0">
+              <h3 className={styles.label}>{item.label}</h3>
+              <p className={styles.desc}>{item.desc}</p>
+            </div>
           </div>
         ))}
       </div>
