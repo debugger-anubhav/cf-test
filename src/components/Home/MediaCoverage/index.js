29cca1cd21f64f5478ca0efbfadb515b245103f4
Pragati-patel <peehupatel1998@gmail.com>
2023-08-26 18:34:24 +0530

    fix media covrage

--- src/components/Home/MediaCoverage/index.js
+++ src/components/Home/MediaCoverage/index.js
@@ -1,38 +1,62 @@
 import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import styles from "./style.module.css";
-import string from "@/constants/Constant.json";
 import {MediaCoverageImages} from "@/constants/constant";
+import string from "@/constants/Constant.json";
 
 const MediaCoverage = () => {
   const str = string.landing_page.Media_coverage;
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 100,
+    autoplay: true,
+    autoplaySpeed: 1000, // Adjust the time interval for the automatic slide
+    slidesToShow: 7,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1460,
+        settings: {
+          slidesToShow: 6,
+        },
+      },
+      {
+        breakpoint: 1260,
+        settings: {
+          slidesToShow: 5,
+        },
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+    ],
+  };
 
   return (
     <div className={styles.media_coverage_wrapper}>
       <p className={styles.label}>{str.label}</p>
       <h1 className={styles.heading}>{str.desc}</h1>
-      <div className={styles.demo}>
-        <div className={styles.icons_div}>
-          {MediaCoverageImages?.map((item, index) => (
-            <img
-              key={index.toString()}
-              className={styles.icon}
-              src={item?.img}
-              alt=""
-            />
-          ))}
-        </div>
-        <div className={styles.icons_div}>
-          {MediaCoverageImages?.map((item, index) => (
-            <img
-              key={index.toString()}
-              className={styles.icon}
-              src={item?.img}
-              alt=""
-            />
-          ))}
-        </div>
-      </div>
+      <Slider {...settings} className={styles.carousel}>
+        {MediaCoverageImages.map((imageUrl, index) => (
+          <div key={index} className={styles.slide}>
+            <img src={imageUrl.img} alt={`Slide ${index}`} />
+          </div>
+        ))}
+      </Slider>
     </div>
   );
 };
+
 export default MediaCoverage;
