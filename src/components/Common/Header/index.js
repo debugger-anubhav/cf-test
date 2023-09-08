fb32a482a751b858eba96c6b2a1a1de1a2c63f55
Rupali <rupali.shethink@gmail.com>
2023-09-08 20:38:38 +0530

    CIT-120 : routing change for cart

--- src/components/Common/Header/index.js
+++ src/components/Common/Header/index.js
@@ -71,9 +71,9 @@ const Header = () => {
 
   const cityId = getLocalStorage("cityId");
 
-  const cartItemsLength = useSelector(
-    state => state.cartPageData.cartItems.length,
-  );
+  // const cartItemsLength = useSelector(
+  //   state => state.cartPageData.cartItems.length,
+  // );
 
   const userId = getLocalStorage("user_id");
   const tempUserId = getLocalStorage("tempUserID");
@@ -174,11 +174,17 @@ const Header = () => {
                   src={Icons.shoppingCard}
                   alt="shopping-card-icon"
                   className={styles.header_shopping_card}
-                  onClick={() => router.push("/cart")}
+                  onClick={() => router.push("https://cityfurnish.com/cart")}
                 />
-                {cartItemsLength > 0 && (
+                {/* <Image
+                  src={Icons.shoppingCard}
+                  alt="shopping-card-icon"
+                  className={styles.header_shopping_card}
+                  onClick={() => router.push("/cart")}
+                /> */}
+                {/* {cartItemsLength > 0 && (
                   <div className={styles.cart_badge}>{cartItemsLength}</div>
-                )}
+                )} */}
               </div>
               {/* </Link> */}
               <Image
