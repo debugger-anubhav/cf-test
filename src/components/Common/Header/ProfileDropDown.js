4ffab3a5737b7a56ec0e341b3d10d31b637fc7c3
hp <deepak.sahu@cityfurnish.com>
2023-09-15 14:38:07 +0530

    adding some fix

--- src/components/Common/Header/ProfileDropDown.js
+++ src/components/Common/Header/ProfileDropDown.js
@@ -50,7 +50,7 @@ export default function ProfileDropDown({setShowProfileDropdown}) {
               localStorage.removeItem("user_name");
               localStorage.removeItem("ci_session");
               setShowProfileDropdown(false);
-              router.push("https://test.rentofurniture.com/cityfurnish");
+              router.push("https://test.rentofurniture.com/logout");
             }
           }}>
           {ele.item}
