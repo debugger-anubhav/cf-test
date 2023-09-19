c2577f38100fc8a3c1f9e72f9324d39a694565e5
aditya-seth-101 <aditya.shetghink@gmail.com>
2023-09-19 15:38:00 +0530

    Swtching branch

--- src/components/Documentation/Sidebar/DocSidebar.js
+++ src/components/Documentation/Sidebar/DocSidebar.js
@@ -1,36 +1,71 @@
-import React from "react";
+import React, {useState} from "react";
 import Link from "next/link";
 import styles from "./docSidebar.module.css";
 
 const DocSidebar = () => {
-  const firstHeadingArr = [{heading: "ssss", link: "ww.com.com"}];
+  const [isActive, setIsActive] = useState();
+
+  const firstContainerArr = [
+    {heading: "My orders", link: "ww.com.com"},
+    {heading: "My Service Requests", link: "ww.com.com"},
+    {heading: "My Payments", link: "ww.com.com"},
+    {heading: "My Invoices", link: "ww.com.com"},
+  ];
+  const secondContainerArr = [
+    {heading: "CF coins", link: "ww.com.com"},
+    {heading: "Referral Code", link: "ww.com.com"},
+  ];
   return (
     <div className={styles.mainContainer}>
-      <div className={styles.fisrtContainer}>
-        <div className={styles.accountText}>Your Account,</div>
-        <div>Pratyush Verma</div>
-        <div>Overview</div>
+      <div className={styles.firstContainer}>
+        <div className={styles.sectionHeadings}>Your Account,</div>
+        <div className={styles.userNameText}> Pratyush Verma</div>
+        <h2 className={`${styles.sectionItems} mt-10`}>Overview</h2>
       </div>
-      <MenuComp list={firstHeadingArr} heading={""} />
+      <MenuComp
+        list={firstContainerArr}
+        heading={"Orders"}
+        setIsActive={setIsActive}
+        isActive={isActive}
+      />
+      <MenuComp
+        list={secondContainerArr}
+        heading={"Benefits"}
+        setIsActive={setIsActive}
+        isActive={isActive}
+      />
     </div>
   );
 };
 
 export default DocSidebar;
 
-const MenuComp = ({list, heading}) => {
+const MenuComp = ({list, heading, isActive, setIsActive}) => {
   return (
-    <>
-      <h2>{heading}</h2>
+    <div className={styles.secondContainer}>
+      <h2 className={styles.sectionHeadings}>{heading}</h2>
       {list?.map(i => {
         return (
           <Link
             key={i.heading}
             href={String(i.link)}
-            className={styles.headingLinks}
-          />
+            className={`${
+              isActive === i.heading
+                ? styles.sectionItemsActive
+                : styles.sectionItems
+            } block`}>
+            <button
+              value={i.heading}
+              onClick={e => {
+                e.preventDefault();
+                console.log(e.target.value);
+                setIsActive(e.target.value);
+              }}>
+              {i.heading}
+            </button>
+          </Link>
         );
       })}
-    </>
+    </div>
   );
 };
