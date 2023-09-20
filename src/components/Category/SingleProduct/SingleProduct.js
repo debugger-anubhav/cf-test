3ce247d89b72d213a6137b27c032fe80b6dd5158
Pragati-patel <peehupatel1998@gmail.com>
2023-09-20 14:38:39 +0530

    fix

--- src/components/Category/SingleProduct/SingleProduct.js
+++ src/components/Category/SingleProduct/SingleProduct.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import InfiniteScroll from "react-infinite-scroll-component";
-import {useParams, useRouter} from "next/navigation";
+import {useRouter} from "next/navigation";
 import style from "./style.module.css";
 
 // import Card from "@/components/Common/HomePageCards";
@@ -34,7 +34,6 @@ const FrequentlyAskedQuestions = loadable(
 const SingleProduct = ({pageNo, setPageNo}) => {
   const [totalPage, setTotalPage] = useState(1);
   const router = useRouter();
-  const {productname} = useParams();
   const dispatch = useDispatch();
   const categoryPageReduxData = useSelector(state => state.categoryPageData);
 
@@ -74,8 +73,10 @@ const SingleProduct = ({pageNo, setPageNo}) => {
   };
 
   const data =
-    productname === "all" || categoryPageReduxData?.isAllProduct
-      ? bodyDataAll
+    getLocalStorage("subCategory")?.replace(/"/g, "") === "All" ||
+    categoryPageReduxData?.isAllProduct
+      ? // productname === "all" || categoryPageReduxData?.isAllProduct
+        bodyDataAll
       : bodyData;
 
   const singleItemLength =
