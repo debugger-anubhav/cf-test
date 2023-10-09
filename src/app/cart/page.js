906ae1d25a34856fc30eafb023d15a123a558b42
Pragati-patel <peehupatel1998@gmail.com>
2023-10-09 17:01:58 +0530

    complete skeleton for cart page

--- src/app/cart/page.js
+++ src/app/cart/page.js
@@ -4,8 +4,13 @@ import Header from "@/components/Common/Header";
 import MenuList from "@/components/Common/MenuList";
 import React from "react";
 
-import CartSection from "@/components/Cart";
+// import CartSection from "@/components/Cart";
 import Notifications from "@/components/Common/Notifications/Notification";
+import loadable from "@loadable/component";
+import {CartPageSkeleton} from "@/components/Cart";
+const CartSection = loadable(() => import("@/components/Cart"), {
+  fallback: <CartPageSkeleton />,
+});
 
 const CartPage = () => {
   return (
