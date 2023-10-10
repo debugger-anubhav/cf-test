da25d7cf466e53b1db1be30f50fd4e941dceff0d
Pragati-patel <peehupatel1998@gmail.com>
2023-10-10 12:40:09 +0530

    solve hydration error from main section in refer a friend page

--- src/components/ReferAFriend/ReferAFriend.js
+++ src/components/ReferAFriend/ReferAFriend.js
@@ -3,23 +3,23 @@ import Header from "../Common/Header";
 import loadable from "@loadable/component";
 import MenuList from "../Common/MenuList";
 import MainSection from "./MainSection/MainSection";
-import DocSidebar from "../Documentation/Sidebar/DocSidebar";
-import {decrypt} from "@/hooks/cryptoUtils";
-import {getLocalStorage} from "@/constants/constant";
+// import DocSidebar from "../Documentation/Sidebar/DocSidebar";
+// import {decrypt} from "@/hooks/cryptoUtils";
+// import {getLocalStorage} from "@/constants/constant";
 
 const Footer = loadable(() => import("@/components/Common/Footer"));
 
 const ReferAFriend = () => {
-  const isLoogedIn = decrypt(getLocalStorage("_ga"));
+  // const isLoogedIn = decrypt(getLocalStorage("_ga"));
   return (
     <div>
       <Header />
       <MenuList />
       {/* @apply hidden font-Poppins lg:flex gap-16 pr-[70px] xl:pr-[90px]macbook:pr-[122px] 3xl:pr-[160px] 4xl:pr-[160px] -mt-6; */}
       <div className="large_layout flex -mt-6 w-full">
-        <div className="min-w-fit hidden lg:flex h-full">
+        {/* <div className="min-w-fit hidden lg:flex h-full">
           {isLoogedIn && <DocSidebar />}
-        </div>
+        </div> */}
         <div className="w-full">
           <MainSection />
         </div>
