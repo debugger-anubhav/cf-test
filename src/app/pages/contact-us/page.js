c0194164c9d34e84741bfa175b2390696b1cfd4c
Pragati-patel <peehupatel1998@gmail.com>
2023-10-11 19:51:02 +0530

    worked on contact page

---
+++ src/app/pages/contact-us/page.js
@@ -0,0 +1,29 @@
+"use client";
+
+import React from "react";
+import AnnouncementBar from "@/components/Common/AnnouncementBar";
+import Header from "@/components/Common/Header";
+import MenuList from "@/components/Common/MenuList";
+import loadable from "@loadable/component";
+import {FooterSkeleton} from "@/components/Common/Footer";
+import ContactSection from "@/components/ContactUs/ContactSection";
+import OurOffices from "@/components/ContactUs/OurOffices";
+
+const Footer = loadable(() => import("@/components/Common/Footer"), {
+  fallback: <FooterSkeleton />,
+});
+
+const ContactUs = () => {
+  return (
+    <div className="large_layout">
+      <AnnouncementBar />
+      <Header />
+      <MenuList />
+      <ContactSection />
+      <OurOffices />
+      <Footer />
+    </div>
+  );
+};
+
+export default ContactUs;
