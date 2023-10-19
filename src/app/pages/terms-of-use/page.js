24f6a1ecd4ee316f1d5a2e3ac4f6dc6c63c8a670
Pragati-patel <peehupatel1998@gmail.com>
2023-10-19 11:24:43 +0530

    term and condition page create

---
+++ src/app/pages/terms-of-use/page.js
@@ -0,0 +1,21 @@
+"use client";
+
+import React from "react";
+import AnnouncementBar from "@/components/Common/AnnouncementBar";
+import Header from "@/components/Common/Header";
+import MenuList from "@/components/Common/MenuList";
+import loadable from "@loadable/component";
+import {FooterSkeleton} from "@/components/Common/Footer";
+const Footer = loadable(() => import("@/components/Common/Footer"), {
+  fallback: <FooterSkeleton />,
+});
+export default function TermsOfUse() {
+  return (
+    <div className="large_layout">
+      <AnnouncementBar />
+      <Header />
+      <MenuList />
+      <Footer />
+    </div>
+  );
+}
