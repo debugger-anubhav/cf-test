a2c6969b25a30a4b4a573c6db0dfcfb8f8a764fa
Pragati-patel <peehupatel1998@gmail.com>
2023-10-25 16:39:53 +0530

    career page

--- src/components/Careers/WeValue/index.js
+++ src/components/Careers/WeValue/index.js
@@ -1,23 +1,24 @@
 import React from "react";
 import styles from "./style.module.css";
+import {Career} from "@/assets/images";
 
 export default function WeValue() {
   const Data = [
     {
       title: "Expert insights",
-      bg: "https://d3juy0zp6vqec8.cloudfront.net/images/expert-insights.webp",
+      bg: Career + "/expert-insights.webp",
     },
     {
       title: "Bold ideas",
-      bg: "https://d3juy0zp6vqec8.cloudfront.net/images/bold-ideas.webp",
+      bg: Career + "/bold-ideas.webp",
     },
     {
       title: "Intellectual courage",
-      bg: "https://d3juy0zp6vqec8.cloudfront.net/images/intellectual-courage.webp",
+      bg: Career + "/intellectual-courage.webp",
     },
     {
       title: "Creativity",
-      bg: "https://d3juy0zp6vqec8.cloudfront.net/images/creativity.webp",
+      bg: Career + "/creativity.webp",
     },
   ];
   return (
@@ -31,7 +32,7 @@ export default function WeValue() {
                 index !== Data.length - 1 ? "lg:mr-6 mr-4" : "mr-0"
               }`}
               key={index.toString()}>
-              <img src={item.bg} alt="Snow" className={"w-full"} />
+              <img src={item.bg} alt={item.title} className={"w-full"} />
               <p className={styles.bottom_left}>{item.title}</p>
             </div>
           );
