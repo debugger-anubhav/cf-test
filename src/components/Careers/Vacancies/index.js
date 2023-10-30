69766397260dd202bdd2d9393aad07fcd55296fe
Pragati-patel <peehupatel1998@gmail.com>
2023-10-30 12:16:45 +0530

    add skeleton for vacancie on career page

--- src/components/Careers/Vacancies/index.js
+++ src/components/Careers/Vacancies/index.js
@@ -4,12 +4,14 @@ import FAQQuestion from "@/components/ReferAFriend/MainSection/FAQQuestion";
 import axios from "axios";
 import {baseURL} from "@/network/axios";
 import {endPoints} from "@/network/endPoints";
+import {Skeleton} from "@mui/material";
 
 export default function Vacancies() {
   const para =
     "Through constant learning, you will grow your career and the scope of your impact across industries. Our culture demands hard work and rewards it with exceptional opportunities. If you're looking for an opportunity to work in high- impact teams, where you can truly develop your skill set and knowledge and bring impact to the business, Cityfurnish is looking for you!";
   const [openIndex, setOpenIndex] = useState(0);
   const [vacaniesData, setVacaniesData] = useState(null);
+  const [loadingSkeleton, setLoadingSkeleton] = useState(true);
   const toggleQuestion = index => {
     if (openIndex === index) {
       setOpenIndex(null);
@@ -23,6 +25,7 @@ export default function Vacancies() {
       .then(res => {
         console.log(res?.data?.data, "response");
         setVacaniesData(res?.data?.data);
+        setLoadingSkeleton(false);
       })
       .catch(err => console.log(err));
   };
@@ -35,24 +38,49 @@ export default function Vacancies() {
     <div className={styles.main_container}>
       <p className={styles.heading}>Vacancies</p>
       <p className={styles.para}>{para}</p>
-      <div className={styles.QuesAnsArray_div}>
-        {vacaniesData?.map((item, index) => {
-          return (
-            <div key={index.toString()}>
-              <FAQQuestion
-                ques={item?.Posting_Title}
-                ans={item?.Job_Description}
-                isOpen={index === openIndex}
-                toggleQuestion={() => toggleQuestion(index)}
-                applyBtn={true}
-              />
-              {index < vacaniesData?.length - 1 && (
-                <div className="bg-EDEDEE h-[1px] w-full" />
-              )}
-            </div>
-          );
-        })}
+      <div>
+        {loadingSkeleton ? (
+          <VacanciesSkeleton />
+        ) : (
+          <div className={styles.QuesAnsArray_div}>
+            {vacaniesData?.map((item, index) => {
+              return (
+                <div key={index.toString()}>
+                  <FAQQuestion
+                    ques={item?.Posting_Title}
+                    ans={item?.Job_Description}
+                    isOpen={index === openIndex}
+                    toggleQuestion={() => toggleQuestion(index)}
+                    applyBtn={true}
+                  />
+                  {index < vacaniesData?.length - 1 && (
+                    <div className={styles.divider} />
+                  )}
+                </div>
+              );
+            })}
+          </div>
+        )}
       </div>
     </div>
   );
 }
+
+export const VacanciesSkeleton = () => {
+  return (
+    <div className="my-10">
+      {[1, 2, 3, 4]?.map(item => {
+        return (
+          <div key={item.toString()} className="my-4">
+            <Skeleton
+              variant="text"
+              className={styles.skeleton_for_text}
+              height={20}
+            />
+            {item < 4 && <div className={styles.skeleton_divider} />}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
