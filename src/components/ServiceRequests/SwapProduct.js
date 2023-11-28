33874b26a945e842b962716cc27bb2b645cff80f
Pragati-patel <peehupatel1998@gmail.com>
2023-11-28 13:40:25 +0530

    set screen for swap product

--- src/components/ServiceRequests/SwapProduct.js
+++ src/components/ServiceRequests/SwapProduct.js
@@ -19,57 +19,65 @@ function SwapProduct() {
     },
   ];
   const [searchModalOpen, setsearchModalOpen] = useState(false);
+  const [showSwapScreen, setShowSwapScreen] = useState(1);
   return (
     <div className={styles.content_wrapper}>
       <div className={styles.main_heading}>
-        <BackIcon />
+        <BackIcon
+          onClick={() => {
+            showSwapScreen === 2 && setShowSwapScreen(1);
+          }}
+          className={"cursor-pointer"}
+        />
         Swap product
       </div>
-      <div className={styles.buy_info}>
-        <p className={styles.desc}>Select products to swap</p>
-
-        <div>
-          {ProductInfo?.map((item, index) => (
-            <div
-              key={index.toString()}
-              className={` ${
-                index !== ProductInfo.length - 1
-                  ? " border-b border-EDEDEE"
-                  : "border-0"
-              } ${styles.request_info_div}`}>
-              <div className="flex gap-2 items-center">
-                <img
-                  className={styles.product_imge_thambnil}
-                  src=""
-                  alt="icon"
-                />
-                <p className={styles.request_type}>{item.title}</p>
+      <div
+        className={`${styles.swap_first_screen} ${
+          showSwapScreen === 1 ? "flex flex-col" : "hidden"
+        } `}>
+        <div className={styles.buy_info}>
+          <p className={styles.desc}>Select products to swap</p>
+          <div>
+            {ProductInfo?.map((item, index) => (
+              <div
+                key={index.toString()}
+                className={` ${
+                  index !== ProductInfo.length - 1
+                    ? " border-b border-EDEDEE"
+                    : "border-0"
+                } ${styles.request_info_div}`}
+                onClick={() => setShowSwapScreen(2)}>
+                <div className="flex gap-2 items-center">
+                  <img
+                    className={styles.product_imge_thambnil}
+                    src=""
+                    alt="icon"
+                  />
+                  <p className={styles.request_type}>{item.title}</p>
+                </div>
+                <div className="flex">
+                  <ForwardArrow />
+                </div>
               </div>
-              <div className="flex">
-                <ForwardArrow />
-              </div>
-            </div>
-          ))}
-        </div>
-        <div className={styles.bottom_row}>
-          <button className={styles.proceed_btn}>
-            Create request <ForwardArrowWithLine />
-          </button>
+            ))}
+          </div>
         </div>
       </div>
 
-      <div className={styles.selected_product_info_wrapper}>
-        <div className="flex flex-col">
-          <p className={styles.desc}>Selected product:</p>
-          <p className={`${styles.desc} !text-222`}>Jane Queen Size Bed</p>
+      <div
+        className={`${styles.swap_second_screen} ${
+          showSwapScreen === 2 ? "flex flex-col" : "hidden"
+        }`}>
+        <div className={styles.selected_product_info_wrapper}>
+          <div className="flex flex-col">
+            <p className={styles.desc}>Selected product:</p>
+            <p className={`${styles.desc} !text-222`}>Jane Queen Size Bed</p>
+          </div>
+          <div className={styles.swap_info}>
+            <img className={styles.product_imge_thambnil} />
+            <IoIosSwap color="#9A9AA2" size={22} />
+          </div>
         </div>
-        <div className={styles.swap_info}>
-          <img className={styles.product_imge_thambnil} />
-          <IoIosSwap color="#9A9AA2" size={22} />
-        </div>
-      </div>
-
-      <div className="w-full">
         <p className={styles.swap_heading}>Select product to swap to</p>
         <div className="relative">
           <div className={styles.search_wrapper}>
@@ -97,6 +105,11 @@ function SwapProduct() {
             </div>
           )}
         </div>
+      </div>
+      <div className={styles.bottom_row}>
+        <button className={styles.proceed_btn}>
+          Create request <ForwardArrowWithLine />
+        </button>
       </div>
     </div>
   );
