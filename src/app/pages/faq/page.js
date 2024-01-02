193fd10362e622d0ec65fccc0763e7c3fc44f957
Pragati-patel <peehupatel1998@gmail.com>
2024-01-02 19:03:46 +0530

    solve trello tikit for career page

--- src/app/pages/faq/page.js
+++ src/app/pages/faq/page.js
@@ -1,10 +1,12 @@
 "use client";
 import React from "react";
 import FAQMain from "@/components/FAQ/FAQ";
+import AnnouncementBar from "@/components/Common/AnnouncementBar";
 
 const FAQ = () => {
   return (
     <div className="large_layout">
+      <AnnouncementBar />
       <FAQMain />
     </div>
   );
