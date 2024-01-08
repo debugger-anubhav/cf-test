227955d3613505af64ee8c3814fa86cdc98ceb50
Pragati-patel <peehupatel1998@gmail.com>
2024-01-08 14:54:01 +0530

    madd layout files for adding meta data to the some pages

---
+++ src/app/pages/refer-a-friend/layout.js
@@ -0,0 +1,36 @@
+import React from "react";
+import PropTypes from "prop-types";
+import ReduxProvider from "@/store/provider";
+import QueryProvider from "@/components/QueryProvider/QueryProvider";
+
+export const metadata = {
+  title: "Cityfurnish- Refer a friend and get extra benefits",
+  description: "Cityfurnish Friends Referral Program",
+};
+
+export default function RootLayout({children}) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body>
+        <ReduxProvider>
+          <QueryProvider>{children}</QueryProvider>
+        </ReduxProvider>
+      </body>
+    </html>
+  );
+}
+RootLayout.propTypes = {
+  children: PropTypes.element,
+};
