ab7d52416b914b7db1169585b44c929d2efac3d8
Pragati-patel <peehupatel1998@gmail.com>
2024-01-09 12:15:00 +0530

    add announcement bar

--- src/components/BulkOrder/BulkOrder.js
+++ src/components/BulkOrder/BulkOrder.js
@@ -4,10 +4,12 @@ import Header from "../Common/Header";
 import MenuList from "../Common/MenuList";
 import Footer from "../Common/Footer";
 import MainWrapper from "./mainWrapper";
+import AnnouncementBar from "../Common/AnnouncementBar";
 
 const BulkOrderMain = () => {
   return (
     <div className="large_layout">
+      <AnnouncementBar />
       <Header />
       <MenuList />
       <MainWrapper />
