52495507117b6419ceb8e9a335c6b5b80ecb230d
Pragati-patel <peehupatel1998@gmail.com>
2024-01-09 15:42:20 +0530

    add meta data

--- src/app/pages/rentalagreement/layout.js
+++ src/app/pages/rentalagreement/layout.js
@@ -13,6 +13,7 @@ export default function RootLayout({children}) {
   return (
     <html lang="en">
       <head>
+        <meta name="Title" content="Cityfurnish Sample Rental Agreement" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
