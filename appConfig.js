270dd9eac5f55b4f6f96d9628d201cbef5173b91
Rupali <rupali.shethink@gmail.com>
2024-01-11 12:31:31 +0530

    fixes

--- appConfig.js
+++ appConfig.js
@@ -25,7 +25,8 @@ export const AppConfig = {
 
 export const cityUrl = "https://d3juy0zp6vqec8.cloudfront.net/images/icons/";
 
-export const razorpayKeyOwn = "rzp_test_8xqfCdTnoga4Yi";
+// export const razorpayKeyOwn = "rzp_test_8xqfCdTnoga4Yi";
+export const razorpayKeyOwn = "rzp_test_dw32j6ggDS3ktY";
 export const razorpayKey = "rzp_test_dw32j6ggDS3ktY";
 
 export const RazorpayThemeColor = "#3E688E";
