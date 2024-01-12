8849d5c2ea048839f19491277f4d1fe724e9b378
Pragati-patel <peehupatel1998@gmail.com>
2024-01-12 18:20:35 +0530

    changes in meta tag

--- src/components/UpfrontPayment/index.js
+++ src/components/UpfrontPayment/index.js
@@ -14,7 +14,7 @@ import {
   setTransactionReferenceNumber,
 } from "@/store/Slices";
 import {useDispatch} from "react-redux";
-
+import {FaCircleInfo} from "react-icons/fa6";
 function UpfrontPayment() {
   const router = useRouter();
   const dispatch = useDispatch();
@@ -109,42 +109,59 @@ function UpfrontPayment() {
   return (
     <div className={styles.wrapper}>
       <p className={styles.main_heading}>{Heading}</p>
-      <div className={styles.upfront_data}>
-        <p className={styles.heading}>
-          Your Order ID
-          {apiData ? (
-            <span className={styles.heading_span}>: #{apiData?.order_id}</span>
-          ) : (
-            <Skeleton variant="text" width={100} className="ml-3" />
-          )}
-        </p>
-        <p className={styles.heading}>
-          Tenure
-          {apiData ? (
-            <span className={styles.heading_span}>: {apiData?.tenure}</span>
-          ) : (
-            <Skeleton variant="text" width={100} className="ml-3" />
-          )}
-        </p>
-        <p className={styles.heading}>
-          Upfront amount
-          {apiData ? (
-            <span className={styles.heading_span}>
-              {" "}
-              : <span className="font-Inter">₹</span>
-              {apiData?.amount}
-            </span>
-          ) : (
-            <Skeleton variant="text" width={100} className="ml-3" />
-          )}
-        </p>
-      </div>
+      {apiData?.payment_status === 2 ? (
+        <>
+          <FaCircleInfo
+            color="#FD6"
+            className="lg:mt-8 mt-6 lg:h-[64px] lg:w-[64px] w-[48px] h-[48px]"
+          />
+          <p className="text-45454A  lg:text-24 text-20 font-Poppins font-medium tracking-[-0.48px] lg:my-8 my-6">
+            Looks like payment has already been made.
+          </p>
+        </>
+      ) : (
+        <div className={styles.upfront_data}>
+          <p className={styles.heading}>
+            Your Order ID
+            {apiData ? (
+              <span className={styles.heading_span}>
+                : #{apiData?.order_id}
+              </span>
+            ) : (
+              <Skeleton variant="text" width={100} className="ml-3" />
+            )}
+          </p>
+          <p className={styles.heading}>
+            Tenure
+            {apiData ? (
+              <span className={styles.heading_span}>: {apiData?.tenure}</span>
+            ) : (
+              <Skeleton variant="text" width={100} className="ml-3" />
+            )}
+          </p>
+          <p className={styles.heading}>
+            Upfront amount
+            {apiData ? (
+              <span className={styles.heading_span}>
+                {" "}
+                : <span className="font-Inter">₹</span>
+                {apiData?.amount}
+              </span>
+            ) : (
+              <Skeleton variant="text" width={100} className="ml-3" />
+            )}
+          </p>
+        </div>
+      )}
+
       <div>
         <button
           disabled={apiData?.payment_status === 2}
-          className={styles.pay_now_btn}
+          className={`${styles.pay_now_btn} ${
+            apiData?.payment_status === 2 && "!mt-0"
+          } `}
           onClick={handleOpenRazorpay}>
-          {apiData?.payment_status === 2 ? "Payment Already Paid" : "Pay now"}
+          {apiData?.payment_status === 2 ? "Return to home page" : "Pay now"}
           {apiData?.payment_status !== 2 && (
             <ForwardArrowWithLine size={20} color={"#222222"} />
           )}
