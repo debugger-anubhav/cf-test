3b7a771cd52581f29f0d86bb61ed3ce772b00548
Pragati-patel <peehupatel1998@gmail.com>
2024-01-16 11:05:05 +0530

    add meta data to the bbulkorder page

---
+++ src/app/pages/bulkorder/layout.js
@@ -0,0 +1,38 @@
+import React from "react";
+import PropTypes from "prop-types";
+import ReduxProvider from "@/store/provider";
+import QueryProvider from "@/components/QueryProvider/QueryProvider";
+
+export const metadata = {
+  title: "Cityfurnish | Bulk Order",
+  description:
+    "Place Bulk Order. Office Furniture, Hotel Furniture, Restaurant Furniture",
+};
+
+export default function RootLayout({children}) {
+  return (
+    <html lang="en">
+      <head>
+        <meta name="Title" content="Cityfurnish | Bulk Order" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body>
+        <ReduxProvider>
+          <QueryProvider>{children}</QueryProvider>
+        </ReduxProvider>
+      </body>
+    </html>
+  );
+}
+RootLayout.propTypes = {
+  children: PropTypes.element,
+};
