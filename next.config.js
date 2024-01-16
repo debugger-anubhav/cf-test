05df87893fe271c4a3adaf3e5e247d296b9d05d4
Pragati-patel <peehupatel1998@gmail.com>
2024-01-16 15:08:26 +0530

    changes

--- next.config.js
+++ next.config.js
@@ -1,22 +1,10 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {};
 
 module.exports = {
   images: {
     domains: ["d3juy0zp6vqec8.cloudfront.net"],
   },
   distDir: "build",
-
-  // added
-  // head: {
-  //   meta: [
-  //     {
-  //       name: "viewport",
-  //       content:
-  //         "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no",
-  //     },
-  //   ],
-  // },
 };
 
 // module.exports = nextConfig;
