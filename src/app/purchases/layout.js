0d4c7b684f5d776ad8fc4f36336b92e7f4f8618e
Pragati-patel <peehupatel1998@gmail.com>
2024-01-17 15:22:56 +0530

    add layout files for adding different meta data

---
+++ src/app/purchases/layout.js
@@ -0,0 +1,41 @@
+import React from "react";
+import PropTypes from "prop-types";
+import ReduxProvider from "@/store/provider";
+import QueryProvider from "@/components/QueryProvider/QueryProvider";
+
+export const metadata = {
+  title: "Purchases",
+  description:
+    "Furniture rental made easy with CityFurnish. Rent home furniture online, including living room furniture, for as long as you need it. Available in major Indian cities like Noida, Delhi, Hyderabad, Mumbai, Gurgaon, Pune and Bangalore. Download our furniture rental app for convenience and flexibility.",
+};
+
+export default function RootLayout({children}) {
+  return (
+    <html lang="en">
+      <head>
+        <meta
+          name="Title"
+          content="Rent Premium Furniture & Home Appliances Online - Cityfurnish"
+        />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body>
+        <ReduxProvider>
+          <QueryProvider>{children}</QueryProvider>
+        </ReduxProvider>
+      </body>
+    </html>
+  );
+}
+RootLayout.propTypes = {
+  children: PropTypes.element,
+};
