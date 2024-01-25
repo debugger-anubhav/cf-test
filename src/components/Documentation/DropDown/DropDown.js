fd26ccd5353c429fc80b8e85b46faa68b10f1bb1
Rupali <rupali.shethink@gmail.com>
2024-01-25 17:13:58 +0530

    CIT-120: kyc fixes, cityshield date fixes

--- src/components/Documentation/DropDown/DropDown.js
+++ src/components/Documentation/DropDown/DropDown.js
@@ -1,6 +1,9 @@
 import React from "react";
 import styles from "./DropDown.module.css";
 import {DownPopUpArrow, PopUpArrow} from "@/assets/icon";
+// import {Drawer, Modal} from "@mui/material";
+// import commonStyles from "../common.module.css";
+// import SelectionCircle from "../SelectionCircle/SelectionCircle";
 const DropDown = ({
   handleSelectChange,
   selectedOption,
@@ -20,54 +23,65 @@ const DropDown = ({
     setIsDDOpen(false);
     isInitialScreen && handleKycState(option);
   };
-  return (
-    <div
-      className={`${styles["custom-select"]} ${isOpen ? "active" : ""}`}
-      // style={isOpen ? {boxShadow: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)"} : {}}
-      style={{
-        maxWidth,
-        boxShadow: isOpen ? "0px 4px 4px 0px rgba(0, 0, 0, 0.25)" : "",
-      }}>
-      <div
-        className={`mt-1 ${styles["selected-option"]} ${
-          isOpen ? "border-none" : "border-[#DDDDDF]"
-        }`}
-        onClick={() => {
-          setIsDDOpen(prev => !prev);
-        }}>
-        <span
-          className={` ${styles.selected_txt} ${
-            selectedOption?.value || selectedOption?.dealCodeNumber
-              ? "text-black"
-              : "text-[#71717A]"
-          }`}>
-          {isInitialScreen
-            ? selectedOption?.dealCodeNumber || "Select order"
-            : selectedOption?.label || "Select an option"}
-        </span>
-        <div className={`${styles.ddArrow}`}>
-          {isOpen ? (
-            <PopUpArrow size={25} className={`${styles.pointer}`} />
-          ) : (
-            <DownPopUpArrow size={25} className={`${styles.pointer}`} />
-          )}
-        </div>
-      </div>
+  // const [isBottomDrawer, setIsBottomDrawer] = useState(false);
+  // const [selectedOptionCur, setSelectedOptionCur] = useState();
 
-      <ul
-        className={`${isOpen ? optionsActive : styles.options} max-h-[260px]`}>
-        {options?.map((option, index) => (
-          <li
-            className={`${styles.option} ${
-              index === options.length - 1 ? "rounded-b-xl border-none" : ""
-            } ${index === 0 ? "border-t" : ""}`}
-            key={index}
-            onClick={() => handleOptionClick(option)}>
-            {isInitialScreen ? option.dealCodeNumber : option.label}
-          </li>
-        ))}
-      </ul>
-    </div>
+  return (
+    <>
+      <div
+        className={`${styles["custom-select"]} ${
+          isOpen ? "md:shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]" : ""
+        }`}
+        // style={isOpen ? {boxShadow: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)"} : {}}
+        style={{
+          maxWidth,
+          // boxShadow: isOpen ? "0px 4px 4px 0px rgba(0, 0, 0, 0.25)" : "",
+        }}>
+        <div
+          className={`mt-1 rounded-xl border-[#DDDDDF] ${
+            styles["selected-option"]
+          } ${isOpen ? "!rounded-b-none" : ""}`}
+          onClick={() => {
+            setIsDDOpen(prev => !prev);
+          }}>
+          <span
+            className={` ${styles.selected_txt} ${
+              selectedOption?.value || selectedOption?.dealCodeNumber
+                ? "text-black"
+                : "text-[#71717A]"
+            }`}>
+            {isInitialScreen
+              ? selectedOption?.dealCodeNumber || "Select order"
+              : selectedOption?.label || "Select an option"}
+          </span>
+          <div className={`${styles.ddArrow}`}>
+            {isOpen ? (
+              <PopUpArrow size={25} className={`${styles.pointer}`} />
+            ) : (
+              <DownPopUpArrow size={25} className={`${styles.pointer}`} />
+            )}
+          </div>
+        </div>
+
+        <ul
+          className={`${isInitialScreen && styles.dropdown} ${
+            isOpen ? styles.optionsActive : styles.options
+          } max-h-[260px] overflow-y-scroll ${
+            isInitialScreen && isOpen && "!block"
+          }`}>
+          {options?.map((option, index) => (
+            <li
+              className={`${styles.option} ${
+                index === options.length - 1 ? "rounded-b-xl border-none" : ""
+              } ${index === 0 ? "border-t" : ""}`}
+              key={index}
+              onClick={() => handleOptionClick(option)}>
+              {isInitialScreen ? option.dealCodeNumber : option.label}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 };
 
