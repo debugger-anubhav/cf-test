bffa8e1285944ad2323a1ac222e7a2f7f92ec786
Rupali <rupali.shethink@gmail.com>
2024-02-05 16:27:20 +0530

    CIT-120: loading implemented at razorpay

---
+++ src/components/Common/Loader/LoaderComponent.js
@@ -0,0 +1,23 @@
+import React from "react";
+import BeatLoader from "react-spinners/BeatLoader";
+import styles from "./styles.module.css";
+// import gif from "./loader.gif";
+// import Image from "next/image";
+
+const LoaderComponent = ({loading}) => {
+  console.log("in loading");
+  return (
+    <div className={` ${styles.loader_container}`}>
+      {/* <Image src={gif} alt="loading" height={"200"} width={"200"} /> */}
+      <BeatLoader
+        color={"#EFF5FF"}
+        loading={loading}
+        //   cssOverride={override}
+        size={20}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+      />
+    </div>
+  );
+};
+export default LoaderComponent;
