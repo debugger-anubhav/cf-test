5315f621d6bb918e7911967bb9b9ddb133537f2d
Pragati-patel <peehupatel1998@gmail.com>
2024-02-09 16:37:51 +0530

    uninstall package

--- src/components/Payments/index.js
+++ src/components/Payments/index.js
@@ -12,7 +12,7 @@ import {endPoints} from "@/network/endPoints";
 import {decrypt} from "@/hooks/cryptoUtils";
 import {getLocalStorage} from "@/constants/constant";
 import {Skeleton} from "@mui/material";
-import html2pdf from "html2pdf.js";
+// import html2pdf from "html2pdf.js";
 
 const PaymentPage = () => {
   const userId = decrypt(getLocalStorage("_ga"));
@@ -34,17 +34,20 @@ const PaymentPage = () => {
         setLoadingSkeleton(false);
       });
   };
-  const generatePdf = htmlContent => {
-    const opt = {
-      margin: 1,
-      filename: "invoice.pdf",
-      image: {type: "jpeg", quality: 0.98},
-      html2canvas: {scale: 2},
-      jsPDF: {unit: "in", format: "letter", orientation: "portrait"},
-    };
+  // const generatePdf = htmlContent => {
+  //   if (typeof window !== "undefined") {
 
-    html2pdf().from(htmlContent).set(opt).save();
-  };
+  //     const opt = {
+  //       margin: 1,
+  //       filename: "invoice.pdf",
+  //       image: {type: "jpeg", quality: 0.98},
+  //       html2canvas: {scale: 2},
+  //       jsPDF: {unit: "in", format: "letter", orientation: "portrait"},
+  //     };
+
+  //     html2pdf().from(htmlContent).set(opt).save();
+  //   }
+  // };
 
   const downloadAPI = () => {
     axios
@@ -61,7 +64,7 @@ const PaymentPage = () => {
       )
       .then(res => {
         console.log(res?.data?.htmlData);
-        generatePdf(res?.data?.htmlData);
+        // generatePdf(res?.data?.htmlData);
       })
       .catch(err => console.log(err));
   };
