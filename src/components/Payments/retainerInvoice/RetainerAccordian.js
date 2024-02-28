08369e5c65af01a840fbe4860c3f40e2a6953a93
Pragati-patel <peehupatel1998@gmail.com>
2024-02-28 15:17:18 +0530

    fix accordian

--- src/components/Payments/retainerInvoice/RetainerAccordian.js
+++ src/components/Payments/retainerInvoice/RetainerAccordian.js
@@ -5,71 +5,64 @@ import {format} from "date-fns";
 
 const RetianerAccordian = ({rows, handleShowMore, visibleRows}) => {
   const [indexOfActiveAcc, setIndexOfActiveAcc] = useState(null);
-  const [showAccSummary, setshowAccSummary] = useState(false);
-
+  const toggleAccordion = index => {
+    setIndexOfActiveAcc(indexOfActiveAcc === index ? null : index);
+  };
   return (
     <div>
       {rows?.slice(0, visibleRows)?.map((row, index) => {
+        const isActive = index === indexOfActiveAcc;
         return (
           <div
             key={index.toString()}
-            className={`${
-              index === indexOfActiveAcc && showAccSummary && " mt-4"
-            }`}>
+            className={`${isActive && styles.active} ${isActive && "mt-4"}`}>
             <div
               className={`flex justify-between w-full px-3 pt-6 cursor-pointer items-center ${
-                index === indexOfActiveAcc && showAccSummary
-                  ? "pb-3 !pt-3 bg-F7F7F8"
-                  : "pb-6"
+                isActive ? "pb-3 !pt-3 bg-F7F7F8" : "pb-6"
               }`}
-              onClick={() => {
-                setIndexOfActiveAcc(index);
-                setshowAccSummary(!showAccSummary);
-              }}>
+              onClick={() => toggleAccordion(index)}>
               <p className={`${styles.tableHeaderCell}`}>
                 Payment Number: {row.payment_number}
               </p>
-              {index === indexOfActiveAcc && showAccSummary ? (
+              {isActive ? (
                 <Minus className={`${styles.exapnd_icon}`} />
               ) : (
                 <Plus className={styles.exapnd_icon} />
               )}
             </div>
-
-            <div
-              className={`w-full flex-col px-3 cursor-pointer ${
-                index === indexOfActiveAcc && showAccSummary
-                  ? "flex bg-F7F7F8"
-                  : "hidden"
-              }`}
-              onClick={() => setshowAccSummary(!showAccSummary)}>
-              <div className={styles.tableCell}>
-                <span className="font-medium">Invoice Date:</span> {row.date}
+            {isActive && (
+              <div
+                className={`w-full flex-col px-3 cursor-pointer ${
+                  isActive ? "flex bg-F7F7F8" : "hidden"
+                }`}>
+                <div className={styles.tableCell}>
+                  <span className="font-medium">Invoice Date:</span> {row.date}
+                </div>
+                <div className={styles.tableCell}>
+                  <span className="font-medium"> Invoice Id:</span>{" "}
+                  {row.invoice_numbers ? row.invoice_numbers : "NA"}
+                </div>
+                <div className={styles.tableCell}>
+                  <span className="font-medium"> Amount Paid:</span>{" "}
+                  {row.amount}
+                </div>
+                <div className={styles.tableCell}>
+                  <span className="font-medium"> Payment Date:</span>{" "}
+                  {`${format(
+                    new Date(row.created_time),
+                    "d LLL, yyyy : hh:mm a",
+                  )}`}
+                </div>
+                <div className={styles.tableCell}>
+                  <span className="font-medium">Payment Number: </span>
+                  {row.payment_number}
+                </div>
               </div>
-              <div className={styles.tableCell}>
-                <span className="font-medium"> Invoice Id:</span>{" "}
-                {row.invoice_numbers ? row.invoice_numbers : "NA"}
-              </div>
-              <div className={styles.tableCell}>
-                <span className="font-medium"> Amount Paid:</span> {row.amount}
-              </div>
-              <div className={styles.tableCell}>
-                <span className="font-medium"> Payment Date:</span>{" "}
-                {`${format(
-                  new Date(row.created_time),
-                  "d LLL, yyyy : hh:mm a",
-                )}`}
-              </div>
-              <div className={styles.tableCell}>
-                <span className="font-medium">Payment Number: </span>
-                {row.payment_number}
-              </div>
-            </div>
-
+            )}
             {index !== rows.length - 1 && (
               <div
                 className={`bg-EDEDEE h-[1px] w-full ${
-                  index === indexOfActiveAcc && showAccSummary && "mt-4"
+                  isActive && "mt-4"
                 }`}></div>
             )}
           </div>
