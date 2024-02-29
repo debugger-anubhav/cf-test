6ae4317480c3785c781a16caa6fd30b003210ab9
Rupali <rupali.shethink@gmail.com>
2024-02-29 13:04:34 +0530

    CIT-120: post screen data fixes for tenure extension

--- src/components/TenureExtension/Cards/MidTermCard.js
+++ src/components/TenureExtension/Cards/MidTermCard.js
@@ -35,9 +35,9 @@ function MidTermCard({
   const router = useRouter();
   const dispatch = useDispatch();
 
-  const calculatedPrice =
-    apiData?.orignalPrice -
-    ((apiData?.orignalPrice * items?.percent_off) / 100).toFixed(0);
+  // const calculatedPrice =
+  //   apiData?.orignalPrice -
+  //   ((apiData?.orignalPrice * items?.percent_off) / 100).toFixed(0);
 
   const DiscountPoints = [
     "No Cost EMI Available",
@@ -124,12 +124,7 @@ function MidTermCard({
       image: "https://rentofurniture.com/images/logo/FaviconNew.png",
       handler: function (res) {
         console.log(res, "res in handler");
-        cartTypeOneHandler(
-          res,
-          customerId,
-          selectedOptionPer?.value * calculatedPrice,
-          recId,
-        );
+        cartTypeOneHandler(res, customerId, apiData?.totalPrice, recId);
       },
       prefill: {
         name: result?.data?.data?.data?.full_name,
