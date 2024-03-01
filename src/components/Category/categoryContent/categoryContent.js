da1a2d1b2dd798bd34daf4e742c70104b5356177
Pragati-patel <peehupatel1998@gmail.com>
2024-03-01 13:20:39 +0530

    add right city id for google review

--- src/components/Category/categoryContent/categoryContent.js
+++ src/components/Category/categoryContent/categoryContent.js
@@ -1,3 +1,52 @@
+// import React, {useEffect} from "react";
+// import styles from "./style.module.css";
+// import {useQuery} from "@/hooks/useQuery";
+// import {endPoints} from "@/network/endPoints";
+// import {getLocalStorage} from "@/constants/constant";
+// import {addCategoryTextContent} from "@/store/Slices/categorySlice";
+// import {useDispatch, useSelector} from "react-redux";
+
+// const CategoryContent = ({subCategoryId}) => {
+//   let cityIdStr;
+//   // let categoryId;
+//   const dispatch = useDispatch();
+
+//   const categoryPageReduxData = useSelector(state => state.categoryPageData);
+
+//   if (typeof window !== "undefined") {
+//     // categoryId = getLocalStorage("categoryId");
+//     cityIdStr = getLocalStorage("cityId");
+//   }
+//   const cityId = parseFloat(cityIdStr);
+
+//   const {refetch: getCategoryText} = useQuery(
+//     "category-content",
+//     endPoints.categoryContent,
+//     `?cityId=${cityId}&categoryId=${subCategoryId}`,
+//   );
+
+//   useEffect(() => {
+//     getCategoryText()
+//       .then(res => dispatch(addCategoryTextContent(res?.data?.data)))
+//       .catch(err => console.log(err));
+//   }, []);
+
+//   return (
+//     <div className={styles.wrapper}>
+//       {categoryPageReduxData?.categorTextContent?.map((ele, index) => {
+//         return (
+//           <div
+//             className={styles.dynamic_keyword}
+//             dangerouslySetInnerHTML={{__html: ele?.cat_meta_keyword}}>
+//           </div>
+//         );
+//       })}
+//     </div>
+//   );
+// };
+
+// export default CategoryContent;
+
 import React, {useEffect} from "react";
 import styles from "./style.module.css";
 import {useQuery} from "@/hooks/useQuery";
@@ -8,13 +57,11 @@ import {useDispatch, useSelector} from "react-redux";
 
 const CategoryContent = ({subCategoryId}) => {
   let cityIdStr;
-  // let categoryId;
   const dispatch = useDispatch();
 
   const categoryPageReduxData = useSelector(state => state.categoryPageData);
 
   if (typeof window !== "undefined") {
-    // categoryId = getLocalStorage("categoryId");
     cityIdStr = getLocalStorage("cityId");
   }
   const cityId = parseFloat(cityIdStr);
@@ -31,15 +78,26 @@ const CategoryContent = ({subCategoryId}) => {
       .catch(err => console.log(err));
   }, []);
 
+  const removeInlineStyles = html => {
+    const tempElement = document.createElement("div");
+    tempElement.innerHTML = html;
+    const elements = tempElement.querySelectorAll("*");
+    elements.forEach(function (element) {
+      element.removeAttribute("style");
+    });
+    return tempElement.innerHTML;
+  };
+
   return (
     <div className={styles.wrapper}>
       {categoryPageReduxData?.categorTextContent?.map((ele, index) => {
         return (
-          <>
-            <div
-              dangerouslySetInnerHTML={{__html: ele?.cat_meta_keyword}}
-              className={styles.dynamic_keyword}></div>
-          </>
+          <div
+            key={index} // Assuming index can be used as key since no unique identifier provided
+            className={styles.dynamic_keyword}
+            dangerouslySetInnerHTML={{
+              __html: removeInlineStyles(ele?.cat_meta_keyword),
+            }}></div>
         );
       })}
     </div>
