900e03174d1fb5dc0d358dd6acb40af0a19fae59
Pragati-patel <peehupatel1998@gmail.com>
2024-03-04 11:47:33 +0530

    relocation data change

--- src/components/ServiceRequests/Relocation.js
+++ src/components/ServiceRequests/Relocation.js
@@ -461,22 +461,6 @@ function Relocation({prevScreen, data}) {
                 </div> */}
                 </div>
               </div>
-              {/* <div className={styles.bottom_row}>
-            <button
-              type="submit"
-              className={`${styles.proceed_btn} bg-none ${
-                !formik.isValid
-                  ? "!bg-[#FFDF85] !cursor-not-allowed"
-                  : `!bg-F6B704`
-              }`}
-              onClick={() => {
-                if (!formik.isValid) {
-                  console.log("errors", formik.errors);
-                }
-              }}>
-              Create request <ForwardArrowWithLine />
-            </button>
-          </div> */}
             </Form>
           )}
         </Formik>
