7f231069e632bd274910826eecbd2ea2dde5a6ed
Pragati-patel <peehupatel1998@gmail.com>
2024-03-07 14:46:18 +0530

    add dynamic metadata to the category page

--- src/app/[city]/[category]/page.js
+++ src/app/[city]/[category]/page.js
@@ -1,34 +1,58 @@
-// export async function getServerSideProps() {
-//   try {
-//     const data = await create();
-//     return {
-//       props: {
-//         data
-//       }
-//     };
-//   } catch (error) {
-//     console.error("Error fetching data:", error);
-//     return {
-//       props: {
-//         data: null
-//       }
-//     };
-//   }
-// }
 import React from "react";
 import CatAnnouncement from "./CatAnnouncement";
 import CatHeader from "./CatHeader";
 import CatMenu from "./CatMenu";
 import CatSubHeader from "./CatSubHeader";
+import {endPoints} from "@/network/endPoints";
+import CryptoJS from "crypto-js";
+export async function getServerSideProps(context) {
+  const {nameOfCity, category} = context.params;
+  try {
+    const data = await create(nameOfCity, category);
+    return {
+      props: {
+        data,
+      },
+    };
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return {
+      props: {
+        data: null,
+      },
+    };
+  }
+}
 
-async function create() {
-  const data = "hello";
-  return data;
+async function create(params) {
+  console.log("dhfsdhfsd", params);
+
+  const tempSecretKey = "b3ad5950f7c555c664f19c9ec77bbfb943";
+  const plaintext = `${Date.now()}/Cityfurnish@India@123!/${Date.now()}`;
+  const createEncryptedHash = (text, secretKey) => {
+    const encrypted = CryptoJS.AES.encrypt(text, secretKey).toString();
+    return encrypted;
+  };
+  const apiKey = createEncryptedHash(plaintext, tempSecretKey);
+
+  const data = await fetch(
+    "https://test.rentofurniture.com/api/" + endPoints.categoryMetaData,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Apikey: apiKey,
+      },
+      body: JSON.stringify({
+        cityName: params.city?.toLowerCase(),
+        seourl: params.category,
+      }),
+    },
+  );
+  return data.json();
 }
 
 export default async function Page() {
-  const data = await create();
-  console.log(data);
   return (
     <>
       <CatAnnouncement />
@@ -37,4 +61,13 @@ export default async function Page() {
       <CatSubHeader />
     </>
   );
+}
+
+export async function generateMetadata({params}) {
+  const data = await create(params);
+  console.log("metadtatadtdatdtadatatd", data.data);
+  return {
+    title: data?.data?.cat_meta_title,
+    description: data?.data?.cat_meta_desc,
+  };
 }
