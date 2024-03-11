3837b0fd277b82ee9e2ed207c7f021db946d234d
Pragati-patel <peehupatel1998@gmail.com>
2024-03-11 15:16:16 +0530

    remove my order page's layout and render them to the server side for meta tags

---
+++ src/app/purchases/SsrPurchases.js
@@ -0,0 +1,3 @@
+"use client";
+import PurchasesComponents from "./PurchasesComponents";
+export default PurchasesComponents;
--- src/app/purchases/page.js
+++ src/app/purchases/page.js
@@ -1,28 +1,33 @@
-"use client";
-import React, {useEffect} from "react";
-import AnnouncementBar from "@/components/Common/AnnouncementBar";
-import Header from "@/components/Common/Header";
-import MenuList from "@/components/Common/MenuList";
-import Notifications from "@/components/Common/Notifications/Notification";
-import MyOrders from "@/components/MyOrders";
-import {AuthProvider} from "@/components/HOC/index";
-import {useDispatch} from "react-redux";
-import {setDocSidebarActiveItem} from "@/store/Slices";
+import React from "react";
+import PurchasesComponents from "./SsrPurchases";
 
-const index = () => {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(setDocSidebarActiveItem("My Orders"));
-  }, []);
+export default async function PurchasesPage() {
   return (
-    <div className="large_layout">
-      <AnnouncementBar />
-      <Header />
-      <MenuList />
-      <MyOrders />
-      <Notifications />
-    </div>
+    <>
+      <meta
+        name="Title"
+        content="Rent Premium Furniture & Home Appliances Online - Cityfurnish"
+      />
+      <PurchasesComponents />
+    </>
   );
-};
+}
 
-export default AuthProvider(index);
+export async function generateMetadata() {
+  const title = "Purchases";
+  const description =
+    "Rent furniture and home appliances online from India's leading furniture rental company Cityfurnish. We offer furniture rental in Bangalore Mumbai, Pune, Delhi, Gurgaon, Noida and Hyderabad.";
+  return {
+    title,
+    description,
+    alternates: {
+      canonical: `https://cityfurnish.com/purchases`,
+    },
+    openGraph: {
+      url: `https://cityfurnish.com/purchases`,
+      title,
+      description,
+      siteName: "Cityfurnish",
+    },
+  };
+}
