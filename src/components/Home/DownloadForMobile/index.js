92414b098b563d3e4c06f277063ec17d0727aa23
Pragati-patel <peehupatel1998@gmail.com>
2024-03-28 19:04:15 +0530

    fixes in download image section on landing page

--- src/components/Home/DownloadForMobile/index.js
+++ src/components/Home/DownloadForMobile/index.js
@@ -1,26 +1,27 @@
 import React from "react";
 import styles from "./style.module.css";
 import {DownloadForMobileImg, DownloadForWeb} from "@/assets/images";
+import Image from "next/image";
 
 const DownloadForMobile = () => {
   return (
     <div className={styles.main_container}>
       <div className={styles.web_container}>
-        <img
+        <Image
           src={DownloadForWeb}
           alt="download-image-for-web"
           className={styles.download_image}
-          width={"100%"}
-          height={"100%"}
+          width={2854}
+          height={1104}
         />
       </div>
       <div className={styles.mobile_container}>
-        <img
+        <Image
           src={DownloadForMobileImg}
           alt="download-image-for-mobile"
           className={styles.download_image}
-          width={"100%"}
-          height={"100%"}
+          width={720}
+          height={1227}
         />
       </div>
     </div>
