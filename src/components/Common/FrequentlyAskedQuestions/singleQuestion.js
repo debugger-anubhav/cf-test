b08ce011f9335083eed6cb75992a0524baeea12d
Pragati-patel <peehupatel1998@gmail.com>
2024-04-02 14:28:00 +0530

    changes in hassel

--- src/components/Common/FrequentlyAskedQuestions/singleQuestion.js
+++ src/components/Common/FrequentlyAskedQuestions/singleQuestion.js
@@ -4,26 +4,24 @@ import {Plus, Minus} from "@/assets/icon";
 
 const SingleQuestion = ({ques, ans, isOpen, toggleQuestion}) => {
   return (
-    <>
-      <div className="pb-6">
-        <div className={styles.quesWrapper} onClick={toggleQuestion}>
-          <h3 className={styles.ques}>{ques}</h3>
-          <div>
-            {isOpen ? (
-              <Minus className="cursor-pointer" size={20} color={"#222"} />
-            ) : (
-              <Plus className="cursor-pointer" size={20} color={"#222"} />
-            )}
-          </div>
-        </div>
+    <div className="pb-6">
+      <div className={styles.quesWrapper} onClick={toggleQuestion}>
+        <h3 className={styles.ques}>{ques}</h3>
         <div>
-          <div
-            dangerouslySetInnerHTML={{__html: ans}}
-            className={`${styles.ans} ${isOpen && styles.ans_open}`}
-          />
+          {isOpen ? (
+            <Minus className="cursor-pointer" size={20} color={"#222"} />
+          ) : (
+            <Plus className="cursor-pointer" size={20} color={"#222"} />
+          )}
         </div>
       </div>
-    </>
+      <div>
+        <div
+          dangerouslySetInnerHTML={{__html: ans}}
+          className={`${styles.ans} ${isOpen && styles.ans_open}`}
+        />
+      </div>
+    </div>
   );
 };
 
--- src/components/Home/HasselFreeServicesCards/index.js
+++ src/components/Home/HasselFreeServicesCards/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import style from "./style.module.css";
 import {HasselFreeData} from "@/constants/constant";
+import Image from "next/image";
 
 const HasselFreeServicesCards = () => {
   const Title = "Truly hassle free service";
@@ -14,35 +15,54 @@ const HasselFreeServicesCards = () => {
               <>
                 <div
                   key={index.toString()}
-                  className={`${style.card_wrapper} hidden xl:flex ${
+                  className={`${style.card_wrapper}  ${
                     index === HasselFreeData?.length - 1 && "mr-[16px] lg:mr-0"
                   }`}
-                  style={{
-                    backgroundImage: `url("${data.backgroungImage}")`,
-                    backgroundRepeat: "no-repeat",
-                    backgroundSize: "cover",
-                  }}>
+                  // style={{
+                  //   backgroundImage: `url("${data.backgroungImage}")`,
+                  //   backgroundRepeat: "no-repeat",
+                  //   backgroundSize: "cover",
+                  // }}
+                >
+                  <Image
+                    src={data.backgroungImage}
+                    alt={data?.Heading}
+                    layout="fill"
+                    className={`${style.card_wrapper} hidden xl:flex`}
+                    objectFit="cover"
+                    objectPosition="center"
+                  />
+                  <Image
+                    src={data.bgImgMobile}
+                    alt={data?.Heading}
+                    layout="fill"
+                    className={`${style.card_wrapper} flex xl:hidden`}
+                    objectFit="cover"
+                    objectPosition="center"
+                  />
                   <div className={style.hassel_heading_wrapper}>
                     <p className={style.heading_text}>{data?.Heading}</p>
                     <p className={style.content}>{data?.text}</p>
                   </div>
                 </div>
 
-                <div
+                {/* <div
                   key={index.toString()}
                   className={`${style.card_wrapper} flex xl:hidden ${
                     index === HasselFreeData?.length - 1 && "mr-[16px] lg:mr-0"
                   }`}
-                  style={{
-                    backgroundImage: `url("${data.bgImgMobile}")`,
-                    backgroundRepeat: "no-repeat",
-                    backgroundSize: "cover",
-                  }}>
+                  // style={{
+                  //   backgroundImage: `url("${data.bgImgMobile}")`,
+                  //   backgroundRepeat: "no-repeat",
+                  //   backgroundSize: "cover",
+                  // }}
+                  >
+                  
                   <div className={style.hassel_heading_wrapper}>
                     <p className={style.heading_text}>{data?.Heading}</p>
                     <p className={style.content}>{data?.text}</p>
                   </div>
-                </div>
+                </div> */}
               </>
             );
           })}
