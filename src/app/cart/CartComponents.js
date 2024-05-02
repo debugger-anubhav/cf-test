df95f64e13001047c101c5c472166cfd050f5ff8
Pragati-patel <peehupatel1998@gmail.com>
2024-05-02 13:35:31 +0530

    fix

--- src/app/cart/CartComponents.js
+++ src/app/cart/CartComponents.js
@@ -1,17 +1,21 @@
 "use client";
-import React from "react";
+import React, {useEffect} from "react";
 import AnnouncementBar from "@/components/Common/AnnouncementBar";
 import Header from "@/components/Common/Header";
 import MenuList from "@/components/Common/MenuList";
 import Notifications from "@/components/Common/Notifications/Notification";
 import {CartPageSkeleton} from "@/components/Cart";
 import loadable from "@loadable/component";
+import {setLocalStorage} from "@/constants/constant";
 
 const CartSection = loadable(() => import("@/components/Cart"), {
   fallback: <CartPageSkeleton />,
 });
 
 const CartComponents = () => {
+  useEffect(() => {
+    setLocalStorage("isMonthly", false);
+  }, []);
   return (
     <div className="large_layout">
       <AnnouncementBar />
