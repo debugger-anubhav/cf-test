0dee54178975e1330f83d166e72256f1ce10b0c1
Pragati-patel <peehupatel1998@gmail.com>
2024-05-08 12:43:07 +0530

    fixe

--- src/app/order/confirmation/cart/page.js
+++ src/app/order/confirmation/cart/page.js
@@ -1,29 +1,21 @@
 "use client";
 import React from "react";
-// import AnnouncementBar from "@/components/Common/AnnouncementBar";
-// import Header from "@/components/Common/Header";
-// import MenuList from "@/components/Common/MenuList";
+import AnnouncementBar from "@/components/Common/AnnouncementBar";
+import Header from "@/components/Common/Header";
+import MenuList from "@/components/Common/MenuList";
 
-// import Notifications from "@/components/Common/Notifications/Notification";
+import Notifications from "@/components/Common/Notifications/Notification";
 // import PaymentConfirmation from "@/components/PostCheckout/Confirmation";
-// import RootLayoutSuccess from "./layout";
-// import {getLocalStorage} from "@/constants/constant";
-// import {decrypt} from "@/hooks/cryptoUtils";
 
 const index = () => {
-  // const userId = decrypt(getLocalStorage("_ga"));
-
   return (
-    <div>fsdfdsfdd</div>
-    // <RootLayoutSuccess userId={userId}>
-    //   <div className="large_layout">
-    //     <AnnouncementBar />
-    //     <Header />
-    //     <MenuList />
-    //     <PaymentConfirmation />
-    //     <Notifications />
-    //   </div>
-    // </RootLayoutSuccess>
+    <div className="large_layout">
+      <AnnouncementBar />
+      <Header />
+      <MenuList />
+      {/* <PaymentConfirmation /> */}
+      <Notifications />
+    </div>
   );
 };
 
