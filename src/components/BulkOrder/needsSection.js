fac7b5ea1978f0f6a43017aa298906d9d29e6d73
Pragati-patel <peehupatel1998@gmail.com>
2024-05-10 17:45:41 +0530

    fix

--- src/components/BulkOrder/needsSection.js
+++ src/components/BulkOrder/needsSection.js
@@ -29,7 +29,7 @@ const NeedsSection = () => {
       </div>
 
       <div className="flex w-full">
-        <div className=" grid grid-cols-2 w-full gap-4">
+        <div className=" grid grid-cols-3 md:grid-cols-2 w-full gap-4">
           {data?.map((ele, idx) => {
             return (
               <div key={idx} className="">
@@ -47,7 +47,7 @@ const NeedsSection = () => {
         </div>
       </div>
 
-      <div>
+      <div className="md:mt-12 mt-9">
         <div className={style.custom_heading}>Custom Made Products</div>
         <p className={style.custom_desc}>
           Need something unique? We will make it for you. We provide custom made
