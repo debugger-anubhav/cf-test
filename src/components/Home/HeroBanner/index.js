69c0ccd80f806af745f3b383035d3b466c289415
Pragati-patel <peehupatel1998@gmail.com>
2024-05-13 19:10:22 +0530

    add banners api

--- src/components/Home/HeroBanner/index.js
+++ src/components/Home/HeroBanner/index.js
@@ -7,9 +7,14 @@ import styles from "./style.module.css";
 import {useSelector} from "react-redux";
 import {useRouter} from "next/navigation";
 // import Image from "next/image";
+import {baseInstance} from "@/network/axios";
+import {endPoints} from "@/network/endPoints";
+import {BASEURL} from "../../../../appConfig";
+import {getLocalStorage} from "@/constants/constant";
 
 const HeroBanner = () => {
   const router = useRouter();
+  const cityId = getLocalStorage("cityId");
   const homePageReduxData = useSelector(state => state.homePagedata);
   const [setshowLinkForRentPage, setSetshowLinkForRentPage] = useState(
     homePageReduxData.showAllRentLink,
@@ -39,7 +44,12 @@ const HeroBanner = () => {
         ?.toLowerCase()}/discount-deals`,
     },
   ];
-
+  const getBanners = () => {
+    baseInstance
+      .get(BASEURL + endPoints.getHomeBanners(cityId))
+      .then(res => console.log(res, "pp"))
+      .catch(err => console.log(err));
+  };
   const handleRedirection = link => {
     if (setshowLinkForRentPage) {
       router.push(link);
@@ -49,102 +59,12 @@ const HeroBanner = () => {
   useEffect(() => {
     setSetshowLinkForRentPage(homePageReduxData.showAllRentLink);
   }, [homePageReduxData.showAllRentLink]);
-
+  useEffect(() => {
+    getBanners();
+  }, []);
   return (
     <div>
-      {/* <div className={`${styles.hero_banner_main} landing_page_carousel`}> */}
-      {/* <Carousel showStatus={false} showArrows={false} showThumbs={false}>
-          <div
-            className={styles.hero_banner_cursor_pointer}
-            onClick={() => {
-              handleRedirection();
-            }}>
-            <Image
-              src={HeroBannerImages.HeroBannerImageOne}
-              width={656}
-              height={298}
-              loading="eager"
-              className={styles.carousel_images}
-              alt="hero-banner-1"
-            />
-          </div>
-          <div className={styles.hero_banner_cursor_pointer}
-            onClick={() => {
-              handleRedirection();
-            }}>
-
-          </div>
-          <div
-            className={styles.hero_banner_cursor_pointer}
-            onClick={() => {
-              handleRedirection();
-            }}>
-            <Image
-              src={HeroBannerImages.HeroBannerImageTwo}
-              width={656}
-              height={298}
-              loading="eager"
-              className={styles.carousel_images}
-              alt="hero-banner-2"
-            />
-          </div>
-          <div className={styles.hero_banner_cursor_pointer}>
-            <Image
-              src={HeroBannerImages.HeroBannerImageThree}
-              className={styles.carousel_images}
-              width={656}
-              height={298}
-              loading="eager"
-              alt="hero-banner-3"
-            />
-          </div>
-        </Carousel> */}
-      {/* <Carousel
-          showStatus={false}
-          showArrows={true}
-          showThumbs={false}
-          autoPlay
-          infiniteLoop>
-          {mobileCarouseImg?.map((item, index) => {
-            return (
-              <div
-                className="flex"
-                key={index.toString()}
-                onClick={() => {
-                  handleRedirection();
-                }}>
-                <img
-                  src={item}
-                  alt={"CarouselImg-" + index + 1}
-                  width={656}
-                  height={298}
-                  className="cursor-pointer"
-                />
-              </div>
-            );
-          })}
-        </Carousel> */}
-      {/* </div> */}
-
       <div className={`${styles.hero_banner_wrapper} flex-col`}>
-        {/* <a
-          href={
-            setshowLinkForRentPage
-              ? `/${homePageReduxData?.cityName
-                  .toLowerCase()
-                  .replace(/\//g, "-")}/rent`
-              : "/"
-          }>
-          <Image
-            src={
-              "https://d3juy0zp6vqec8.cloudfront.net/images/cfnewimages/hero-banner-main.webp"
-            }
-            alt="hero-banner"
-            width={1788}
-            height={746}
-            loading="eager"
-          />
-        </a> */}
         <Carousel
           showStatus={false}
           showArrows={true}
