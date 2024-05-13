21dac959c6d33900f8be5648104bf7d368778772
Pragati-patel <peehupatel1998@gmail.com>
2024-05-13 11:07:47 +0530

    add offers in side menu and correct the speclling in script

--- src/app/layout.js
+++ src/app/layout.js
@@ -82,7 +82,7 @@ export default function RootLayout({children}) {
               {
                 "@context": "https://schema.org/",
                 "@type": "Product",
-                "name": "Cityfunrish",
+                "name": "Cityfurnish",
                 "image": "https://d3juy0zp6vqec8.cloudfront.net/images/icons/final-logo.png",
                 "description": "Discover comfort and style with Cityfurnish, India's premier furniture rental brand. We've curated a diverse selection of furniture and furnishings to enhance the style and convenience of your home. Our pieces draw inspiration from the way people live in Indian cities, blending elements from different eras for a unique living experience. Experience the simplicity of renting furniture with Cityfurnish – we provide affordable packages and convenient payment options, ensuring your home is both stylish and hassle-free.",
                 "aggregateRating": {
