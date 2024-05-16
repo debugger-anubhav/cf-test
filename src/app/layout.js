17e2616c08381f66e1fcf737533856f585d02b97
Pragati-patel <peehupatel1998@gmail.com>
2024-05-16 16:40:59 +0530

    revert

--- src/app/layout.js
+++ src/app/layout.js
@@ -63,10 +63,10 @@ export default function RootLayout({children}) {
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800&display=swap"
           rel="stylesheet"
         />
-        {/* <link
+        <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap"
           rel="stylesheet"
-        /> */}
+        />
         <link
           rel="shortcut icon"
           type="image/x-icon"
