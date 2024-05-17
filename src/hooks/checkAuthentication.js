66abcccd934c9ec11b1d97cb14c7237b459b809f
Pragati-patel <peehupatel1998@gmail.com>
2024-05-17 15:19:03 +0530

    isAuth things

--- src/hooks/checkAuthentication.js
+++ src/hooks/checkAuthentication.js
@@ -1,6 +1,9 @@
-import {baseInstance} from "@/network/axios";
-import {endPoints} from "@/network/endPoints";
+// import {baseInstance} from "@/network/axios";
+// import {endPoints} from "@/network/endPoints";
 import Cookies from "universal-cookie";
+import {decrypt} from "@/hooks/cryptoUtils";
+import {getLocalStorage} from "@/constants/constant";
+
 // eslint-disable-next-line camelcase
 // import cookie from "react-cookies";
 
@@ -11,22 +14,25 @@ export const useAuthentication = () => {
   const checkAuthentication = async () => {
     const cookies = new Cookies();
     const token = cookies.get("authToken");
+    const userId = decrypt(getLocalStorage("_ga"));
     // if (!token) {
     //   localStorage.removeItem("user_id");
     //   localStorage.removeItem("_ga");
     // }
     try {
-      const response = await baseInstance.post(
-        endPoints.login.isAuthenticate,
-        {},
-        {
-          headers: {
-            Authorization: token,
-          },
-        },
-      );
+      // const response = await baseInstance.post(
+      //   endPoints.login.isAuthenticate,
+      //   {},
+      //   {
+      //     headers: {
+      //       Authorization: token,
+      //     },
+      //   },
+      // );
 
-      if (response.data.message === true) {
+      // console.log(cookies.get("authToken"),userId,"ooooooooooo")
+
+      if (token && userId) {
         return true;
       } else {
         cookies.remove("authToken", {path: "/"});
