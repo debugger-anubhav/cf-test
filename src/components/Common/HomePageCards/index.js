10e06a340fced3716fe2ca710263011d2c6adb1e
Pragati-patel <peehupatel1998@gmail.com>
2024-05-30 15:47:15 +0530

    changes for bckbutton issue

--- src/components/Common/HomePageCards/index.js
+++ src/components/Common/HomePageCards/index.js
@@ -13,6 +13,7 @@ import {showToastNotification} from "../Notifications/toastUtils";
 import {useAuthentication} from "@/hooks/checkAuthentication";
 import {reduxSetModalState, setLoginPopupState} from "@/store/Slices";
 import LoginModal from "@/components/LoginPopups";
+import {useRouter} from "next/navigation";
 
 const Card = ({
   desc,
@@ -30,6 +31,7 @@ const Card = ({
   seourl,
   isSavedComp = false,
 }) => {
+  const router = useRouter();
   const {checkAuthentication} = useAuthentication();
   const [inWishList, setInWishList] = useState(isSavedComp || false);
   const [hoverCard, setHoverCard] = useState(false);
@@ -165,98 +167,103 @@ const Card = ({
           // addToWishlist();
         }}
       />
-      <a
+      {/* <a
         href={!reduxStateOfLoginPopup && `/things/${productID}/${seourl}`}
-        // onClick={e => e.preventDefault()}
         className={styles.anchor_card}
         aria-label={desc.replace(/-/g, " ")}
         target="_self"
-        rel="noopener">
-        <div
-          // onClick={e => {
-          //   !reduxStateOfLoginPopup && handleProductClick(e, productID, seourl);
-          // }}
-          className={`${styles.wrapper} ${hoverCard && styles.hover_wrapper} ${
-            productWidth ?? ""
-          } 
+        rel="noopener"> */}
+      <div
+        // onClick={e => {
+        //   !reduxStateOfLoginPopup && handleProductClick(e, productID, seourl);
+        // }}
+        onClick={() => {
+          if (!reduxStateOfLoginPopup) {
+            router.push(`/things/${productID}/${seourl}`);
+            window.scrollTo({top: 0, left: 0, behavior: "smooth"});
+          }
+        }}
+        className={`${styles.wrapper} ${hoverCard && styles.hover_wrapper} ${
+          productWidth ?? ""
+        } 
       `}
-          onMouseOver={() => {
-            isHover && setHoverCard(true);
-          }}
-          onMouseOut={() => {
-            setHoverCard(false);
-          }}>
-          <div className="relative">
-            <img
-              src={hoverCard ? hoverCardImage : cardImage}
-              alt={desc}
-              loading="lazy"
-              width={"100%"}
-              height={"100%"}
-              className={`${styles.thumbnail}
+        onMouseOver={() => {
+          isHover && setHoverCard(true);
+        }}
+        onMouseOut={() => {
+          setHoverCard(false);
+        }}>
+        <div className="relative">
+          <img
+            src={hoverCard ? hoverCardImage : cardImage}
+            alt={desc}
+            loading="lazy"
+            width={"100%"}
+            height={"100%"}
+            className={`${styles.thumbnail}
           ${hoverCard && styles.card_image_hover} 
           }
           `}
+          />
+
+          {/* ----------- */}
+          {showincludedItem && (
+            <div className={styles.item_included_container}>
+              <p
+                className={
+                  styles.item_icluded_text
+                }>{`${itemIncluded} items included`}</p>
+            </div>
+          )}
+          {soldOut && (
+            <div className={styles.soldout_tag}>
+              <p className={styles.tag_text}>SOLD OUT</p>
+            </div>
+          )}
+        </div>
+        <div className={styles.desc_div}>
+          <h3 className={styles.desc} style={{lineHeight: "normal"}}>
+            {/* {desc.replace(/-/g, " ")} */}
+            {xlTrimmedString}
+          </h3>
+          <div
+            id={productID}
+            onClick={e => {
+              e.preventDefault();
+              e.stopPropagation();
+              handleWhislistCard(e);
+            }}>
+            <Heart
+              size={25}
+              color={inWishList ? "#D96060" : "#C0C0C6"}
+              className={"cursor-pointer"}
             />
-
-            {/* ----------- */}
-            {showincludedItem && (
-              <div className={styles.item_included_container}>
-                <p
-                  className={
-                    styles.item_icluded_text
-                  }>{`${itemIncluded} items included`}</p>
-              </div>
-            )}
-            {soldOut && (
-              <div className={styles.soldout_tag}>
-                <p className={styles.tag_text}>SOLD OUT</p>
-              </div>
-            )}
-          </div>
-          <div className={styles.desc_div}>
-            <h3 className={styles.desc} style={{lineHeight: "normal"}}>
-              {/* {desc.replace(/-/g, " ")} */}
-              {xlTrimmedString}
-            </h3>
-            <div
-              id={productID}
-              onClick={e => {
-                e.preventDefault();
-                e.stopPropagation();
-                handleWhislistCard(e);
-              }}>
-              <Heart
-                size={25}
-                color={inWishList ? "#D96060" : "#C0C0C6"}
-                className={"cursor-pointer"}
-              />
-            </div>
-          </div>
-          <div className={styles.price_div}>
-            <div className={styles.card_price_wrap}>
-              <h3 className={`${styles.currentPrice} flex`}>
-                <span className={styles.rupeeIcon}>₹</span>
-                {`${currentPrice} /mo`}
-              </h3>
-              {
-                // currentPrice >= originalPrice ? (
-                originalPrice > currentPrice ? (
-                  <h3 className={`${styles.originalPrice} flex`}>
-                    <span className={styles.rupeeIcon}>₹</span>
-                    {`${originalPrice} /mo`}
-                  </h3>
-                ) : null
-              }
-            </div>
-
-            {/* {originalPrice !== currentPrice && ( */}
-            {currentPrice < originalPrice && parseInt(discount) > 0 && (
-              <div className={styles.discount}>{`-${discount} OFF`}</div>
-            )}
           </div>
         </div>
-      </a>
+        <div className={styles.price_div}>
+          <div className={styles.card_price_wrap}>
+            <h3 className={`${styles.currentPrice} flex`}>
+              <span className={styles.rupeeIcon}>₹</span>
+              {`${currentPrice} /mo`}
+            </h3>
+            {
+              // currentPrice >= originalPrice ? (
+              originalPrice > currentPrice ? (
+                <h3 className={`${styles.originalPrice} flex`}>
+                  <span className={styles.rupeeIcon}>₹</span>
+                  {`${originalPrice} /mo`}
+                </h3>
+              ) : null
+            }
+          </div>
+
+          {/* {originalPrice !== currentPrice && ( */}
+          {currentPrice < originalPrice && parseInt(discount) > 0 && (
+            <div className={styles.discount}>{`-${discount} OFF`}</div>
+          )}
+        </div>
+      </div>
+      {/* </a> */}
     </>
   );
 };
