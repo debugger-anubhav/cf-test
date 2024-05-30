08a440532382ecd3402360f1271bec945ba2ff35
Pragati-patel <peehupatel1998@gmail.com>
2024-05-30 12:12:34 +0530

    revert decrypt userid code

--- src/app/layout.js
+++ src/app/layout.js
@@ -98,32 +98,24 @@ export default function RootLayout({children}) {
           dangerouslySetInnerHTML={{
             __html: `
             const userId = localStorage.getItem("_ga");
-            if(!userId && userId!==null){
-              fetch("https://test.rentofurniture.com/ajxapi/getDecryptedUserId", {
-                method: "POST",
-                body: JSON.stringify({
-                  userId: JSON.parse(userId)
-                }),
-                headers: {
-                  
-                }
-              })
-              .then(res => res.json())
-              .then(res => {
-                window.fcWidgetMessengerConfig = {
-                  meta: {
-                    cf_userid: res.data.userId,
-                  },
-                }
-              })
-              .catch(e => console.log('e',e))
-            }else{
+            fetch("https://test.rentofurniture.com/ajxapi/getDecryptedUserId", {
+              method: "POST",
+              body: JSON.stringify({
+                userId: JSON.parse(userId)
+              }),
+              headers: {
+
+              }
+            })
+            .then(res => res.json())
+            .then(res => {
               window.fcWidgetMessengerConfig = {
                 meta: {
-                  cf_userid: "",
+                  cf_userid: res.data.userId,
                 },
               }
-            }
+            })
+            .catch(e => console.log('e',e))
             `,
           }}></script>
         <script src="//in.fw-cdn.com/30445413/247408.js" chat="true"></script>
