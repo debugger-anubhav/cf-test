ccd74a4936021387c5653767224ac5950c5a5a5a
Pragati-patel <peehupatel1998@gmail.com>
2024-05-31 16:29:36 +0530

    set annoucementbar is false in redux for tenure page

--- src/app/upfront_tenure_extension/[recurringId]/page.js
+++ src/app/upfront_tenure_extension/[recurringId]/page.js
@@ -1,15 +1,20 @@
 "use client";
-import React from "react";
+import React, {useEffect} from "react";
 import loadable from "@loadable/component";
 import {FooterSkeleton} from "@/components/Common/Footer";
 import Header from "@/components/Common/Header";
 import MenuList from "@/components/Common/MenuList";
 import TenureExtension from "@/components/TenureExtension";
+import {setAnnouncementBar} from "@/store/Slices";
+import {useDispatch} from "react-redux";
 const Footer = loadable(() => import("@/components/Common/Footer"), {
   fallback: <FooterSkeleton />,
 });
-
 function TenureExtensionPage() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(setAnnouncementBar(true));
+  }, []);
   return (
     <div className="large_layout">
       <Header />
