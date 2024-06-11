7633eb69fb8ca44be13d06ddddf226096048aa8a
Pragati-patel <peehupatel1998@gmail.com>
2024-06-11 16:50:57 +0530

    changes in next config

--- next.config.js
+++ next.config.js
@@ -5,6 +5,50 @@ module.exports = {
     domains: ["d3juy0zp6vqec8.cloudfront.net"],
   },
   distDir: "build",
-};
 
-// module.exports = nextConfig;
+  // module.exports = nextConfig;
+  async redirects() {
+    return [
+      {
+        source: "/user_sign_up/:path*",
+        destination: "/login/:path*",
+        permanent: true,
+      },
+      {
+        source: "/:city/rent-electronics/:path*",
+        destination: "/:city/home-appliances-rental/:path*",
+        permanent: true,
+      },
+      {
+        source: "/:city/living-room-furniture/:path*",
+        destination: "/:city/living-room-furniture-on-rent/:path*",
+        permanent: true,
+      },
+      {
+        source: "/:city/storage-on-rent/:path*",
+        destination: "/:city/home-furniture-rental/:path*",
+        permanent: true,
+      },
+      {
+        source: "/:city/bedroom-furniture/:path*",
+        destination: "/:city/bedroom-furniture-on-rent/:path*",
+        permanent: true,
+      },
+      {
+        source: "/:city/dining-room-furniture/:path*",
+        destination: "/:city/dining-furniture-on-rent/:path*",
+        permanent: true,
+      },
+      {
+        source: "/:city/office-furniture/:path*",
+        destination: "/:city/office-furniture-rental/:path*",
+        permanent: true,
+      },
+      {
+        source: "/shopby/:category/:path*",
+        destination: `/bangalore/:category/:path*?viaShopBy=true`,
+        permanent: true,
+      },
+    ];
+  },
+};
