5b583113014b271a6174da8afcc83281c820c3af
debugger-anubhav <anubhav.kumar98@gmail.com>
2024-07-02 12:46:32 +0530

    fix(*): lint errors with image component

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,30 +1,30 @@
-import React, {useState, useEffect, useRef} from "react";
+import React from "react";
 import NextImage from "next/image";
 
-const getSizes = () =>
-  `(max-width: 359) 359px,
-    (max-width: 427px) 427px,
-    (max-width: 482px) 482px,
-    (max-width: 519px) 519px,
-    (max-width: 929px) 929px,
-    (max-width: 1139px) 1139px,
-    (max-width: 1359px) 1359px,
-    (max-width: 1399px) 1399px,
-    (max-width: 1439px) 1439px,
-    (max-width: 1659px) 1659px,
-    (max-width: 1849px) 1849px,
-    (max-width: 1999px) 1999px,
-  `;
+// const getSizes = () =>
+//   `(max-width: 359) 359px,
+//     (max-width: 427px) 427px,
+//     (max-width: 482px) 482px,
+//     (max-width: 519px) 519px,
+//     (max-width: 929px) 929px,
+//     (max-width: 1139px) 1139px,
+//     (max-width: 1359px) 1359px,
+//     (max-width: 1399px) 1399px,
+//     (max-width: 1439px) 1439px,
+//     (max-width: 1659px) 1659px,
+//     (max-width: 1849px) 1849px,
+//     (max-width: 1999px) 1999px,
+//   `;
 
 const Image = ({src, alt, priority = false, loading = "lazy", ...props}) => {
-  const sizes = getSizes();
+  // const sizes = getSizes();
 
   const formattedUrl = src?.startsWith("https:") ? src : `https:${src}`;
 
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [height, setHeight] = useState(undefined);
+  // const [isLoaded, setIsLoaded] = useState(false);
+  // const [height, setHeight] = useState(undefined);
 
-  const ref = useRef(null);
+  // const ref = useRef(null);
 
   // const handleResize = () => {
   //   setHeight(ref.current.getBoundingClientRect().height);
