59a65c6bfa086d4483ebe8a7ea553215042e3b30
Pragati-patel <peehupatel1998@gmail.com>
2024-07-03 19:20:43 +0530

    uncomment some componet of not found page

--- src/components/NotFound/MainFile.js
+++ src/components/NotFound/MainFile.js
@@ -1,13 +1,13 @@
 "use client";
 import React from "react";
-// import AnnouncementBar from "../Common/AnnouncementBar/index";
 import NotFoundComponent from "./NotFoundComponent/index";
+import AnnouncementBar from "../Common/AnnouncementBar";
 // import Header from "../Common/Header/index";
 
 export default function MainFile() {
   return (
     <div>
-      {/* <AnnouncementBar /> */}
+      <AnnouncementBar />
       {/* <Header /> */}
       <NotFoundComponent />
     </div>
