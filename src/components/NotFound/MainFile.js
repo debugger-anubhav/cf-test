627dd3d158291d3cfd3fe5387e2f0297bbb36742
Pragati-patel <peehupatel1998@gmail.com>
2024-07-03 19:29:25 +0530

    final fixes

--- src/components/NotFound/MainFile.js
+++ src/components/NotFound/MainFile.js
@@ -2,13 +2,13 @@
 import React from "react";
 import NotFoundComponent from "./NotFoundComponent/index";
 import AnnouncementBar from "../Common/AnnouncementBar";
-// import Header from "../Common/Header/index";
+import Header from "../Common/Header/index";
 
 export default function MainFile() {
   return (
     <div>
       <AnnouncementBar />
-      {/* <Header /> */}
+      <Header />
       <NotFoundComponent />
     </div>
   );
