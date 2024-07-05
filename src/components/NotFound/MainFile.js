623ee219c49fec83dfcff5ae4b913bb711223a60
Pragati-patel <peehupatel1998@gmail.com>
2024-07-05 11:13:02 +0530

    fix 404 page issue

--- src/components/NotFound/MainFile.js
+++ src/components/NotFound/MainFile.js
@@ -3,12 +3,15 @@ import React from "react";
 import NotFoundComponent from "./NotFoundComponent/index";
 import AnnouncementBar from "../Common/AnnouncementBar";
 import Header from "../Common/Header/index";
+import MenuList from "../Common/MenuList";
 
 export default function MainFile() {
   return (
     <div>
       <AnnouncementBar />
+
       <Header />
+      <MenuList />
       <NotFoundComponent />
     </div>
   );
