b09fdcd47bbf9dcbb9604d2599088b0889f6ebe0
debugger-anubhav <anubhav.kumar98@gmail.com>
2024-07-08 16:46:39 +0530

    fix(*): formatting issues

--- src/app/layout.js
+++ src/app/layout.js
@@ -153,10 +153,7 @@ export default function RootLayout({children}) {
         />
         <Clarity includeInDevelopment />
 
-        <script
-          src="//in.fw-cdn.com/30445413/247408.js"
-          chat="true"
-        />
+        <script src="//in.fw-cdn.com/30445413/247408.js" chat="true" />
         {/* <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap"
           rel="stylesheet"
