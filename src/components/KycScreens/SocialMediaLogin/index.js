3f06a28545434970300945c178f06af1985d9dfb
Pragati-patel <peehupatel1998@gmail.com>
2024-07-16 18:55:39 +0530

    added social icons

--- src/components/KycScreens/SocialMediaLogin/index.js
+++ src/components/KycScreens/SocialMediaLogin/index.js
@@ -8,6 +8,21 @@ import {signIn} from "next-auth/react";
 export default function SocialMediaLogin() {
   const dispatch = useDispatch();
 
+  const data = [
+    {
+      item: "Facebook",
+      icon: "https://d3juy0zp6vqec8.cloudfront.net/images/cfnewicons/facebook-icn.svg",
+    },
+    {
+      item: "X (Twitter)",
+      icon: "https://d3juy0zp6vqec8.cloudfront.net/images/cfnewicons/twitter-icn.svg",
+    },
+    {
+      item: "LinkedIn",
+      icon: "https://d3juy0zp6vqec8.cloudfront.net/images/cfnewicons/linkedin-icn.svg",
+    },
+  ];
+
   const handleConnect = async () => {
     await signIn("facebook");
   };
@@ -18,16 +33,19 @@ export default function SocialMediaLogin() {
         <BackIcon
           color={"#222222"}
           size={20}
-          onClick={() => dispatch(setKycScreenName("selectOrderId"))}
+          onClick={() => dispatch(setKycScreenName("dashboard"))}
           className={"cursor-pointer"}
         />
-        Personal details
+        Social media login
       </div>
       <div className="flex w-full flex-col gap-6 my-8">
-        {["Facebook", "X (Twitter)", "LinkedIn"]?.map((item, index) => {
+        {data?.map((item, index) => {
           return (
             <div className={styles.content_wrapper} key={index.toString()}>
-              <p className={styles.item}>{item}</p>
+              <div className="flex items-center gap-2">
+                <img src={item.icon} alt={item.item} />
+                <p className={styles.item}>{item?.item}</p>
+              </div>
               <button className={styles.connect_btn} onClick={handleConnect}>
                 Connect
               </button>
