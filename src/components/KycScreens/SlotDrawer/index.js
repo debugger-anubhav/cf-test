7bed711bddb6d4057b967291dacee469e7c53a5e
Pragati-patel <peehupatel1998@gmail.com>
2024-07-22 18:46:20 +0530

    remove commented code

--- src/components/KycScreens/SlotDrawer/index.js
+++ src/components/KycScreens/SlotDrawer/index.js
@@ -148,35 +148,6 @@ const SlotDrawer = ({
   );
 
   return (
-    // <div>
-    //   {isBottomShareDrawer ? (
-    //     <Drawer
-    //       anchor={"bottom"}
-    //       open={isModalOpen}
-    //       onClose={closeModal}
-    //       classes={{paper: styles.bottomDrawer}}
-    //       transitionDuration={{enter: 200, exit: 200}}>
-    //       <div className={styles.close_icon} onClick={closeModal}>
-    //         <Close color={"#45454A"} size={24} className="cursor-pointer" />
-    //       </div>
-    //       <ModalContent />
-    //     </Drawer>
-    //   ) : (
-    //     <Modal
-    //       styles={{}}
-    //       open={isModalOpen}
-    //       onClose={closeModal}
-    //       // center={true}
-    //       classNames={{
-    //         modal: styles.customModal,
-    //         overlay: styles.customOverlay,
-    //         closeButton: styles.customCloseButton,
-    //       }}>
-    //       <ModalContent />
-    //       {loader && <LoaderComponent loading={loader} />}
-    //     </Modal>
-    //   )}
-    // </div>
     <div>
       <Drawer
         anchor={isBottomShareDrawer ? "bottom" : "right"}
