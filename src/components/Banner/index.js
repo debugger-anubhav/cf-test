28794bcc930888b0ccdb0404074f72d304dc318a
umesh <umesh@cityfurnish.com>
2024-07-30 19:16:47 +0530

    rebase

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,17 +1,21 @@
-"use client"
-import React from 'react';
-import styles from "./index.module.css"
+"use client";
+import React from "react";
+import styles from "./index.module.css";
 
-import { productPageImagesBaseUrl } from "@/constants/constant";
-const Banner = ({ image }) => {
-    React.useEffect(() => {
-        console.log(image, "Hello ");
-    }, [])
-    return (
-        <div className={styles.MainContainer}>
-            <img className={styles.BannerImage} src={productPageImagesBaseUrl + image} alt='Banner Image' />
-        </div>
-    );
-}
+import {productPageImagesBaseUrl} from "@/constants/constant";
+const Banner = ({image}) => {
+  React.useEffect(() => {
+    console.log(image, "Hello ");
+  }, []);
+  return (
+    <div className={styles.MainContainer}>
+      <img
+        className={styles.BannerImage}
+        src={productPageImagesBaseUrl + image}
+        alt="Banner Image"
+      />
+    </div>
+  );
+};
 
 export default Banner;
