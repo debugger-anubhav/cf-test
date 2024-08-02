fc99373f181f1003d9ab79af20f6f156d27d6805
umesh <umesh@cityfurnish.com>
2024-08-02 11:43:35 +0530

    sec-one

--- src/components/Common/TextContent/index.js
+++ src/components/Common/TextContent/index.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, {useEffect, useState, useRef} from "react";
 import styles from "../../Category/categoryContent/style.module.css";
 import Worker from "worker-loader!./textContentWorker";
 
-export default function TextContent({ params }) {
+export default function TextContent({params}) {
   const [data, setData] = useState([]);
   const [paramsCityId, setParamsCityId] = useState(46);
   const workerRef = useRef(null);
@@ -11,7 +11,7 @@ export default function TextContent({ params }) {
     // Create worker instance and store it in a ref
     workerRef.current = new Worker();
 
-    workerRef.current.onmessage = function ({ data: { data, cityId } }) {
+    workerRef.current.onmessage = function ({data: {data, cityId}}) {
       if (cityId) {
         setParamsCityId(cityId);
       }
@@ -25,13 +25,13 @@ export default function TextContent({ params }) {
 
   useEffect(() => {
     if (workerRef.current) {
-      workerRef.current.postMessage({ type: "city", params });
+      workerRef.current.postMessage({type: "city", params});
     }
   }, [params?.city]);
 
   useEffect(() => {
     if (workerRef.current) {
-      workerRef.current.postMessage({ params, paramsCityId });
+      workerRef.current.postMessage({params, paramsCityId});
     }
   }, [params, paramsCityId]);
 
@@ -42,7 +42,7 @@ export default function TextContent({ params }) {
           {data.map((item, index) => (
             <div
               className={styles.dynamic_keyword}
-              dangerouslySetInnerHTML={{ __html: item.meta_keyword }}
+              dangerouslySetInnerHTML={{__html: item.meta_keyword}}
               key={index.toString()}
             />
           ))}
@@ -51,14 +51,14 @@ export default function TextContent({ params }) {
       {params.category === "furniture-rental" && (
         <>
           <div
-            dangerouslySetInnerHTML={{ __html: data?.data?.cat_meta_keyword }}
+            dangerouslySetInnerHTML={{__html: data?.data?.cat_meta_keyword}}
             className={styles.dynamic_keyword}
           />
         </>
       )}
       {params.category === "appliances-rental" && (
         <div
-          dangerouslySetInnerHTML={{ __html: data?.cat_meta_keyword }}
+          dangerouslySetInnerHTML={{__html: data?.cat_meta_keyword}}
           className={styles.dynamic_keyword}
         />
       )}
