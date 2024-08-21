393814ba6e6fb19dd3d328eb75d95517df116591
Pragati-patel <peehupatel1998@gmail.com>
2024-08-21 15:18:17 +0530

    remove main file from notfound root file

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,11 +1,7 @@
 import React from "react";
-import MainFile from "../components/NotFound/MainFile";
+// import MainFile from "../components/NotFound/MainFile";
 import RootLayout from "./layout";
 
 export default function NotFound() {
-  return (
-    <RootLayout>
-      <MainFile />
-    </RootLayout>
-  );
+  return <RootLayout>{/* <MainFile /> */}</RootLayout>;
 }
