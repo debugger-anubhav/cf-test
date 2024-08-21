69e8a78f08da4bbbbf9d51ccf3fb2713c19c1430
Pragati-patel <peehupatel1998@gmail.com>
2024-08-21 15:06:01 +0530

    restore not-found.jd file

---
+++ src/app/not-found.js
@@ -0,0 +1,11 @@
+import React from "react";
+import MainFile from "../components/NotFound/MainFile";
+import RootLayout from "./layout";
+
+export default function NotFound() {
+  return (
+    <RootLayout>
+      <MainFile />
+    </RootLayout>
+  );
+}
