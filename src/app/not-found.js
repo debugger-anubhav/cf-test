53899f740e989132e3b382658e45481cf60df775
Pragati-patel <peehupatel1998@gmail.com>
2024-08-21 16:23:47 +0530

    changes for not found

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,12 +1,12 @@
 import React from "react";
 // import MainFile from "../components/NotFound/MainFile";
-import RootLayout from "./layout";
+// import RootLayout from "./layout";
 import NotFoundCatchAll from "./[...not_found]/page";
 
 export default function NotFound() {
   return (
-    <RootLayout>
-      <NotFoundCatchAll />
-    </RootLayout>
+    // <RootLayout>
+    <NotFoundCatchAll />
+    // </RootLayout>
   );
 }
