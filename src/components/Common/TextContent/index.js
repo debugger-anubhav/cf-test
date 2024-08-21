47587141e4959bc71f31a9dd0107a8185e936a5c
raman <raman@cityfurnish.com>
2024-08-21 19:49:24 +0530

    Fix api call for meta data

--- src/components/Common/TextContent/index.js
+++ src/components/Common/TextContent/index.js
@@ -1,48 +1,77 @@
-import React, {useEffect, useState, useRef} from "react";
+import React, { useEffect, useState, useRef } from "react";
 import styles from "../../Category/categoryContent/style.module.css";
 import Worker from "worker-loader!./textContentWorker";
+import { baseInstance } from "@/network/axios";
+import { endPoints } from "@/network/endPoints";
 
-export default function TextContent({params}) {
+export default function TextContent({ params }) {
   const [data, setData] = useState([]);
   const [paramsCityId, setParamsCityId] = useState(46);
   const workerRef = useRef(null);
 
   useEffect(() => {
-    // Create worker instance and store it in a ref
-    workerRef.current = new Worker();
+    // // Create worker instance and store it in a ref
+    // workerRef.current = new Worker();
 
-    workerRef.current.onmessage = function ({data: {data, cityId}}) {
-      if (cityId) {
-        setParamsCityId(cityId);
+    // workerRef.current.onmessage = function ({ data: { data, cityId } }) {
+    //   if (cityId) {
+    //     setParamsCityId(cityId);
+    //   }
+    //   console.log(data, "Hello From Text");
+
+    //   setData(data);
+    // };
+
+    // return () => {
+    //   workerRef.current?.terminate();
+    // };
+
+    baseInstance.get(endPoints.cityIdByCityName + params?.city).then((res) => {
+      if (params?.category === "appliances-rental") {
+        baseInstance
+          .get(
+            `${endPoints.seoAppliancesTextContent}?cityId=${res?.data?.data?.id}&categoryId=26`,
+          )
+          .then(({ data: { data } }) => {
+            setData(data)
+          });
+      } else if (params?.category === "furniture-rental") {
+        baseInstance
+          .get(
+            `${endPoints.seoFurnitureTextContent}?cityId=${res?.data?.data?.id}&categoryId=27`,
+          )
+          .then(({ data }) => {
+            setData(data)
+          });
+      } else {
+        baseInstance.get(endPoints.homePageTextContent).then(({ data: { data } }) => {
+          setData(data)
+        });
       }
-      setData(data);
-    };
+    })
+  }, [params]);
 
-    return () => {
-      workerRef.current?.terminate();
-    };
-  }, []);
+  // useEffect(() => {
+  //   if (workerRef.current) {
+  //     workerRef.current.postMessage({ type: "city", params });
+  //   }
+  // }, [params?.city]);
 
-  useEffect(() => {
-    if (workerRef.current) {
-      workerRef.current.postMessage({type: "city", params});
-    }
-  }, [params?.city]);
-
-  useEffect(() => {
-    if (workerRef.current) {
-      workerRef.current.postMessage({params, paramsCityId});
-    }
-  }, [params, paramsCityId]);
+  // useEffect(() => {
+  //   if (workerRef.current) {
+  //     workerRef.current.postMessage({ params, paramsCityId });
+  //   }
+  // }, [params, paramsCityId]);
 
   return (
+
     <div className={styles.wrapper}>
       {params === "home-page" && (
         <>
-          {data.map((item, index) => (
+          {data?.map((item, index) => (
             <div
               className={styles.dynamic_keyword}
-              dangerouslySetInnerHTML={{__html: item.meta_keyword}}
+              dangerouslySetInnerHTML={{ __html: item?.meta_keyword }}
               key={index.toString()}
             />
           ))}
@@ -51,14 +80,14 @@ export default function TextContent({params}) {
       {params.category === "furniture-rental" && (
         <>
           <div
-            dangerouslySetInnerHTML={{__html: data?.data?.cat_meta_keyword}}
+            dangerouslySetInnerHTML={{ __html: data?.data?.cat_meta_keyword }}
             className={styles.dynamic_keyword}
           />
         </>
       )}
       {params.category === "appliances-rental" && (
         <div
-          dangerouslySetInnerHTML={{__html: data?.cat_meta_keyword}}
+          dangerouslySetInnerHTML={{ __html: data?.cat_meta_keyword }}
           className={styles.dynamic_keyword}
         />
       )}
