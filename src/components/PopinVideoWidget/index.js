83820e4a9bacfb2fbd1a2662f3ef7a05e57702bf
raman <raman@cityfurnish.com>
2024-08-21 15:20:51 +0530

    Add Customer Details

--- src/components/PopinVideoWidget/index.js
+++ src/components/PopinVideoWidget/index.js
@@ -1,38 +1,42 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import styles from "./style.module.css";
-import {BiSolidCameraMovie} from "react-icons/bi";
+import { BiSolidCameraMovie } from "react-icons/bi";
 
-const PopinVideoWidget = () => {
-  useEffect(() => {
-    const popIn = document.createElement("script");
-    popIn.setAttribute("src", "https://widget01.popin.to/js/widget.js");
-    document.body.appendChild(popIn);
+const PopinVideoWidget = ({ productID }) => {
+    const userName = localStorage.getItem("user_name");
+    const userNumber = localStorage.getItem("user_number");
+    const userEmail = localStorage.getItem("user_email");
 
-    popIn.onload = () => {
-      if (window.popInWidgetInit) {
-        window.popInWidgetInit({
-          token: "12159",
-          mode: "hidden",
-        });
-      }
-    };
-    return () => {
-      if (popIn) {
-        document.body.removeChild(popIn);
-      }
-    };
-  }, []);
-  return (
-    <button
-      onClick={() => window.Popin && window.Popin("open")}
-      className={styles.btn1}
-      // ref={addToCartButtonRef}
-    >
-      <BiSolidCameraMovie color="Red" size={22} />
-      <span className="ml-2">Live Product Tour</span>
-      {/* <div className={styles.spinner} /> */}
-    </button>
-  );
+    useEffect(() => {
+        const popIn = document.createElement("script");
+        popIn.setAttribute("src", "https://widget01.popin.to/js/widget.js");
+        document.body.appendChild(popIn);
+        popIn.onload = () => {
+            if (window.popInWidgetInit) {
+                window.popInWidgetInit({
+                    token: "12159",
+                    mode: "hidden", //optional to hide widget icon
+                    captured: {
+                        name: userName,
+                        mobile: userNumber,
+                        email: userEmail,
+                    }
+                });
+            }
+        };
+        return () => {
+            if (popIn) {
+                document.body.removeChild(popIn);
+            }
+        };
+    }, []);
+    return (
+        <button
+            onClick={() => window.Popin && window.Popin("open")} className={styles.btn1}>
+            <BiSolidCameraMovie color="Red" size={22} />
+            <span className="ml-2">Live Product Tour</span>
+        </button>
+    );
 };
 
 export default PopinVideoWidget;
