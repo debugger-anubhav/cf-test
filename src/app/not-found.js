011026b1d83e297202ebed092bc8acd320f1f96e
Pragati-patel <peehupatel1998@gmail.com>
2024-08-21 16:35:09 +0530

    fixes for 404

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,12 +1,20 @@
 import React from "react";
-// import MainFile from "../components/NotFound/MainFile";
+// // import MainFile from "../components/NotFound/MainFile";
 // import RootLayout from "./layout";
+// import NotFoundCatchAll from "./[...not_found]/page";
+
+// export default function NotFound() {
+//   return (
+//     <RootLayout>
+//       <NotFoundCatchAll />
+//     </RootLayout>
+//   );
+// }
+
 import NotFoundCatchAll from "./[...not_found]/page";
 
-export default function NotFound() {
-  return (
-    // <RootLayout>
-    <NotFoundCatchAll />
-    // </RootLayout>
-  );
-}
+const NotFoundPage = () => {
+  return <NotFoundCatchAll />;
+};
+
+export default NotFoundPage;
