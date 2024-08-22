4424b783df78a309073e9093a9e7c1d72f558aa6
Pragati-patel <peehupatel1998@gmail.com>
2024-08-22 15:17:59 +0530

    fix 404 error

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, {useEffect} from "react";
 // // import MainFile from "../components/NotFound/MainFile";
 // import RootLayout from "./layout";
 // import NotFoundCatchAll from "./[...not_found]/page";
@@ -15,6 +16,15 @@ import NotFoundCatchAll from "./[...not_found]/page";
 import ReduxProvider from "@/store/provider";
 import QueryProvider from "@/components/QueryProvider/QueryProvider";
 const NotFoundPage = () => {
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const originalPushState = window.history.pushState;
+      window.history.pushState = function (...args) {
+        originalPushState.apply(window.history, args);
+        window.history.replaceState({...window.history.state, statusCode: 200});
+      };
+    }
+  }, []);
   return (
     <ReduxProvider>
       <QueryProvider>
