45c9db8d5146dcb949f38b05423cf3186c3e52a0
Pragati-patel <peehupatel1998@gmail.com>
2024-08-22 15:39:06 +0530

    changes for 404

--- src/app/[...not_found]/page.js
+++ src/app/[...not_found]/page.js
@@ -1,6 +1,13 @@
 import React from "react";
 import MainFile from "../../components/NotFound/MainFile";
 
-export default function NotFoundCatchAll() {
+export const generateStaticParams = async () => {
+  return []; // No paths, will render 404 with a 200 status code
+};
+
+export default function NotFoundCatchAll({params}) {
+  if (params?.not_found[0] === "404") {
+    return <MainFile />;
+  }
   return <MainFile />;
 }
--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,17 +1,5 @@
 "use client";
 import React, {useEffect} from "react";
-// // import MainFile from "../components/NotFound/MainFile";
-// import RootLayout from "./layout";
-// import NotFoundCatchAll from "./[...not_found]/page";
-
-// export default function NotFound() {
-//   return (
-//     <RootLayout>
-//       <NotFoundCatchAll />
-//     </RootLayout>
-//   );
-// }
-
 import NotFoundCatchAll from "./[...not_found]/page";
 import ReduxProvider from "@/store/provider";
 import QueryProvider from "@/components/QueryProvider/QueryProvider";
