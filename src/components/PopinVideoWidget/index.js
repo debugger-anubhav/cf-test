8b9190c718dd0ff76cdf26ad9d6528a997aa256d
raman <raman@cityfurnish.com>
2024-08-22 19:21:29 +0530

    Fix Video Tool

--- src/components/PopinVideoWidget/index.js
+++ src/components/PopinVideoWidget/index.js
@@ -1,42 +1,45 @@
 import React, { useEffect } from "react";
 import styles from "./style.module.css";
 import { BiSolidCameraMovie } from "react-icons/bi";
+import { GoDotFill } from "react-icons/go";
 
 const PopinVideoWidget = ({ productID }) => {
     const userName = localStorage.getItem("user_name");
     const userNumber = localStorage.getItem("user_number");
     const userEmail = localStorage.getItem("user_email");
 
-    useEffect(() => {
-        const popIn = document.createElement("script");
-        popIn.setAttribute("src", "https://widget01.popin.to/js/widget.js");
-        document.body.appendChild(popIn);
-        popIn.onload = () => {
-            if (window.popInWidgetInit) {
-                window.popInWidgetInit({
-                    token: "12159",
-                    mode: "hidden", //optional to hide widget icon
-                    captured: {
-                        name: userName,
-                        mobile: userNumber,
-                        email: userEmail,
-                    }
-                });
-            }
-        };
-        return () => {
-            if (popIn) {
-                document.body.removeChild(popIn);
-            }
-        };
-    }, []);
     return (
-        <button
-            onClick={() => window.Popin && window.Popin("open")} className={styles.btn1}>
-            <BiSolidCameraMovie color="Red" size={22} />
-            <span className="ml-2">Live Product Tour</span>
-        </button>
+        <>
+            <script
+                src="https://widget01.popin.to/js/widget.js"
+                dangerouslySetInnerHTML={{
+                    __html: `
+            window.onload = function() {
+              if (typeof popInWidgetInit === 'function') {
+                popInWidgetInit({
+                  token: "123456",
+                  mode: "hidden", 
+                  captured: { 
+                    name: ${userName},
+                    mobile: ${userNumber},
+                    email:  ${userEmail}"
+                  }
+                });
+              }
+            };
+          `,
+                }}
+            ></script>
+            <button
+                onClick={() => window.Popin && window.Popin("open")}
+                className={styles.btn1}
+            >
+                <BiSolidCameraMovie color="#597491" size={22} />  <GoDotFill color="red" />|
+                <span className="ml-2">Live Shop</span>
+            </button>
+        </>
     );
 };
 
 export default PopinVideoWidget;
+
