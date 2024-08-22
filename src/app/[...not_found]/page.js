51f22651c8ca4bcacb47f707ff930d9b17772800
Pragati-patel <peehupatel1998@gmail.com>
2024-08-22 15:56:29 +0530

    recreate not-found.js

--- src/app/[...not_found]/page.js
+++ src/app/[...not_found]/page.js
@@ -1,13 +1,6 @@
 import React from "react";
 import MainFile from "../../components/NotFound/MainFile";
 
-export const generateStaticParams = async () => {
-  return []; // No paths, will render 404 with a 200 status code
-};
-
-export default function NotFoundCatchAll({params}) {
-  if (params?.not_found[0] === "404") {
-    return <MainFile />;
-  }
+export default function NotFoundCatchAll() {
   return <MainFile />;
 }
