916b706c8289e20c47a46b6df089f063d7465a39
raman <raman@cityfurnish.com>
2024-08-26 16:12:45 +0530

    fix

--- src/components/ServiceRequests/PastRequestAccordian.js
+++ src/components/ServiceRequests/PastRequestAccordian.js
@@ -103,13 +103,24 @@ const PastRequestAccordian = ({ pastRequestData }) => {
                 </div>
                 <div className={`${styles.tableCell} flex items-center gap-1 whitespace-nowrap`}>
                   <span className="font-medium">Scheduled Date:</span>{" "}
-                  <span className="flex gap-2 whitespace-nowrap">{row?.scheduled_datetime
+                  {/* <span className="flex gap-2 whitespace-nowrap">{row?.scheduled_datetime
                     ? `${format(
                       new Date(row?.scheduled_datetime),
                       "yyyy-MM-dd",
                     )}`
                     : "NA"}
-                    {ShowPop(row?.request_type) && <span onClick={() => handleClick(row?.order_id, row?.zoho_case_id)} className={"cursor-pointer"} ><EditIcon size={18} /> </span>}
+                    {(row?.scheduled_datetime || row?.srScheduledDatetime) && ShowPop(row?.request_type) && <span onClick={() => handleClick(row?.order_id, row?.zoho_case_id)} className={"cursor-pointer"} ><EditIcon size={18} /> </span>}
+                  </span> */}
+                  <span className="flex gap-2 whitespace-nowrap">{row?.scheduled_datetime
+                    ? `${format(
+                      new Date(row?.scheduled_datetime),
+                      "yyyy-MM-dd",
+                    )}`
+                    : row?.srScheduledDatetime ? `${format(
+                      new Date(row?.srScheduledDatetime),
+                      "yyyy-MM-dd",
+                    )}` : "NA"}
+                    {(row?.scheduled_datetime || row?.srScheduledDatetime) && ShowPop(row?.request_type) && <span onClick={() => handleClick(row?.order_id, row?.zoho_case_id)} className={"cursor-pointer"} ><EditIcon size={18} /> </span>}
                   </span>
                 </div>
                 <div className={styles.tableCell}>
